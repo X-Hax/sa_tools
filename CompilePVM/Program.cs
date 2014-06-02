ef73edd27babe9a6b10802ff2fdc608c49c8139b
MainMemory <sonicmike2@yahoo.com>
2014-06-01 23:16:22 -0500

    Adding PAK tools, improving GVM2PAK and CompilePVM.

--- CompilePVM/Program.cs
+++ CompilePVM/Program.cs
@@ -1,90 +1,115 @@
 ﻿using System;
-using System.Drawing;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
 using System.IO;
-
-using PuyoTools.Modules;
 using PuyoTools.Modules.Archive;
-using VrSharp;
 using VrSharp.PvrTexture;
 
 namespace CompilePVM
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string directoryName;
-            UInt32 startingGBIX = 0;
-            List<String> textureNames = new List<String>();
-            List<PvrTexture> finalTextureList = new List<PvrTexture>();
- 
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Error - no texturelist provided. Provide a path to a texturelist as your first command line argument");
-                Console.WriteLine("Press ENTER to continue...");
-                Console.ReadLine();
-                return;
-            }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string directoryName;
+			UInt32 startingGBIX = 0;
+			List<String> textureNames = new List<String>();
+			List<PvrTexture> finalTextureList = new List<PvrTexture>();
 
-            String filePath = args[0];
-            directoryName = Path.GetDirectoryName(filePath);
-            string archiveName = Path.GetFileNameWithoutExtension(filePath);
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Error - no texturelist provided. Provide a path to a texturelist as your first command line argument");
+				Console.WriteLine("Press ENTER to continue...");
+				Console.ReadLine();
+				return;
+			}
 
-            if (File.Exists(filePath))
-            {
-                StreamReader texlistStream = File.OpenText(filePath);
+			String filePath = args[0];
+			directoryName = Path.GetDirectoryName(filePath);
+			string archiveName = Path.GetFileNameWithoutExtension(filePath);
 
-                startingGBIX = UInt32.Parse(texlistStream.ReadLine());
+			if (File.Exists(filePath))
+			{
+				StreamReader texlistStream = File.OpenText(filePath);
 
-                while (!texlistStream.EndOfStream)
-                {
-                    string newName = texlistStream.ReadLine();
-                    textureNames.Add(newName);
-                }
+				startingGBIX = UInt32.Parse(texlistStream.ReadLine());
 
-                PvmArchive pvmArchive = new PvmArchive();
-                Stream pvmStream = File.Open(String.Concat(directoryName, String.Format("//{0}.pvm", archiveName)), FileMode.Create);
-                PvmArchiveWriter pvmWriter = (PvmArchiveWriter)pvmArchive.Create(pvmStream);
+				while (!texlistStream.EndOfStream)
+					textureNames.Add(texlistStream.ReadLine());
 
-                // Reading in textures
-                for (uint imgIndx = 0; imgIndx < textureNames.Count; imgIndx++)
-                {
-                    Bitmap tempTexture = new Bitmap(8, 8);
-                    string texturePath = String.Concat(directoryName, String.Format("//{0}.png", textureNames[(int)imgIndx]));
-                    if (File.Exists(texturePath))
-                    {
-                        tempTexture = (Bitmap)Bitmap.FromFile(texturePath);
-                    }
-                    else
-                    {
-                        Console.WriteLine(String.Concat("Texture ", textureNames[(int)imgIndx], " not found. Generating a placeholder. Check your files."));
-                    }
+				PvmArchive pvmArchive = new PvmArchive();
+				Stream pvmStream = File.Open(Path.ChangeExtension(filePath, ".pvm"), FileMode.Create);
+				PvmArchiveWriter pvmWriter = (PvmArchiveWriter)pvmArchive.Create(pvmStream);
 
-                    PvrTextureEncoder encoder = new PvrTextureEncoder(tempTexture, PvrPixelFormat.Argb4444, PvrDataFormat.Rectangle); // currently using the most wasteful format. Do checks to make this more accurate later.
-                    encoder.GlobalIndex = startingGBIX + imgIndx;
-                    encoder.Save(String.Concat(directoryName, String.Format("//{0}.pvr", textureNames[(int)imgIndx])));
+				// Reading in textures
+				for (uint imgIndx = 0; imgIndx < textureNames.Count; imgIndx++)
+				{
+					Bitmap tempTexture = new Bitmap(8, 8);
+					string texturePath = Path.Combine(directoryName, Path.ChangeExtension(textureNames[(int)imgIndx], ".png"));
+					if (File.Exists(texturePath))
+					{
+						tempTexture = (Bitmap)Bitmap.FromFile(texturePath);
+						tempTexture = tempTexture.Clone(new Rectangle(Point.Empty, tempTexture.Size), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+					}
+					else
+					{
+						Console.WriteLine(String.Concat("Texture ", textureNames[(int)imgIndx], " not found. Generating a placeholder. Check your files."));
+					}
 
-                    pvmWriter.CreateEntryFromFile(String.Concat(directoryName, String.Format("//{0}.pvr", textureNames[(int)imgIndx])));
-                }
+					System.Drawing.Imaging.BitmapData bmpd = tempTexture.LockBits(new Rectangle(Point.Empty, tempTexture.Size), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+					int stride = bmpd.Stride;
+					byte[] bits = new byte[Math.Abs(stride) * bmpd.Height];
+					System.Runtime.InteropServices.Marshal.Copy(bmpd.Scan0, bits, 0, bits.Length);
+					tempTexture.UnlockBits(bmpd);
+					int tlevels = 0;
+					for (int y = 0; y < tempTexture.Height; y++)
+					{
+						int srcaddr = y * Math.Abs(stride);
+						for (int x = 0; x < tempTexture.Width; x++)
+						{
+							Color c = Color.FromArgb(BitConverter.ToInt32(bits, srcaddr + (x * 4)));
+							if (c.A == 0)
+								tlevels = 1;
+							else if (c.A < 255)
+							{
+								tlevels = 2;
+								break;
+							}
+						}
+						if (tlevels == 2)
+							break;
+					}
+					PvrPixelFormat ppf = PvrPixelFormat.Rgb565;
+					if (tlevels == 1)
+						ppf = PvrPixelFormat.Argb1555;
+					else if (tlevels == 2)
+						ppf = PvrPixelFormat.Argb4444;
+					PvrDataFormat pdf = PvrDataFormat.Rectangle;
+					if (tempTexture.Width == tempTexture.Height)
+						pdf = PvrDataFormat.SquareTwiddled;
+					PvrTextureEncoder encoder = new PvrTextureEncoder(tempTexture, ppf, pdf);
+					encoder.GlobalIndex = startingGBIX + imgIndx;
+					string pvrPath = Path.ChangeExtension(texturePath, ".pvr");
+					encoder.Save(pvrPath);
 
-                pvmWriter.Flush();
-                pvmStream.Close();
+					pvmWriter.CreateEntryFromFile(pvrPath);
+				}
 
-                Console.WriteLine("PVM was compiled successfully!");
-                Console.WriteLine("Press ENTER to continue...");
-                Console.ReadLine();
-                return;
-            }
-            else // error, supplied path is invalid
-            {
-                Console.WriteLine("Supplied texturelist does not exist!");
-                Console.WriteLine("Press ENTER to continue...");
-                Console.ReadLine();
-                return;
-            }
-        }
-    }
+				pvmWriter.Flush();
+				pvmStream.Close();
+
+				Console.WriteLine("PVM was compiled successfully!");
+				Console.WriteLine("Press ENTER to continue...");
+				Console.ReadLine();
+				return;
+			}
+			else // error, supplied path is invalid
+			{
+				Console.WriteLine("Supplied texturelist does not exist!");
+				Console.WriteLine("Press ENTER to continue...");
+				Console.ReadLine();
+				return;
+			}
+		}
+	}
 }
---
+++ PAKtool/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using PAKLib;
+
+namespace PAKtool
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                args = new string[] { "/?" };
+            switch (args[0].ToLowerInvariant())
+            {
+                case "/t":
+                    Main(ParseCommandLine(File.ReadAllText(args[1])));
+                    break;
+                case "/u":
+                    try
+                    {
+                        string fn = Path.Combine(Environment.CurrentDirectory, args[1]);
+                        Environment.CurrentDirectory = Path.GetDirectoryName(fn);
+                        PAKFile pak = new PAKFile(fn);
+                        Dictionary<string, PAKInfo> list = new Dictionary<string, PAKInfo>(pak.Files.Count);
+                        foreach (PAKFile.File item in pak.Files)
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(Path.Combine(Environment.CurrentDirectory, item.Name)));
+                            File.WriteAllBytes(item.Name, item.Data);
+                            list.Add(item.Name, new PAKInfo(item.LongPath));
+                        }
+                        IniFile.Serialize(list, Path.ChangeExtension(fn, "ini"));
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                    break;
+                case "/p":
+                    try
+                    {
+                        string fn = Path.Combine(Environment.CurrentDirectory, args[1]);
+                        Environment.CurrentDirectory = Path.GetDirectoryName(fn);
+                        Dictionary<string, PAKInfo> list = IniFile.Deserialize<Dictionary<string, PAKInfo>>(Path.ChangeExtension(fn, "ini"));
+                        PAKFile pak = new PAKFile();
+                        foreach (KeyValuePair<string, PAKInfo> item in list)
+                            pak.Files.Add(new PAKFile.File(item.Key, item.Value.LongPath, File.ReadAllBytes(item.Key)));
+                        pak.Save(Path.ChangeExtension(fn, "pak"));
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                    break;
+                case "/?":
+                    Console.WriteLine("Arguments:");
+                    Console.WriteLine();
+                    Console.WriteLine("/?\tShow this help.");
+                    Console.WriteLine();
+                    Console.WriteLine("/t filename\tReads text file filename as a commandline.");
+                    Console.WriteLine();
+                    Console.WriteLine("/u filename\tExtracts files from an archive.");
+                    Console.WriteLine();
+                    Console.WriteLine("/p filename\tPacks files into an archive.");
+                    Console.WriteLine();
+                    break;
+                default:
+                    if (args.Length == 0) goto case "/?";
+                    char arg = '\0';
+                    while (arg != 'u' & arg != 'p')
+                    {
+                        Console.Write("Type u to unpack or p to pack: ");
+                        arg = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+                    }
+                    Main(new string[] { "/" + arg, args[0] });
+                    break;
+            }
+        }
+
+        public static string[] ParseCommandLine(string commandLine)
+        {
+            List<string> args2 = new List<string>();
+            string curcmd = string.Empty;
+            bool quotes = false;
+            foreach (char item in commandLine)
+            {
+                switch (item)
+                {
+                    case ' ':
+                        if (!quotes)
+                        {
+                            if (!string.IsNullOrEmpty(curcmd))
+                                args2.Add(curcmd);
+                            curcmd = string.Empty;
+                        }
+                        else
+                            goto default;
+                        break;
+                    case '"':
+                        if (quotes)
+                        {
+                            args2.Add(curcmd);
+                            curcmd = string.Empty;
+                            quotes = false;
+                        }
+                        else
+                            quotes = true;
+                        break;
+                    default:
+                        curcmd += item;
+                        break;
+                }
+            }
+            if (!string.IsNullOrEmpty(curcmd))
+                args2.Add(curcmd);
+            return args2.ToArray();
+        }
+    }
+
+    public class PAKInfo
+    {
+        public PAKInfo() { }
+
+        public PAKInfo(string longpath) { LongPath = longpath; }
+
+        public string LongPath { get; set; }
+    }
+}
