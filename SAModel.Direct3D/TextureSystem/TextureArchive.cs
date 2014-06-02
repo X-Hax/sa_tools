fb3005226a1ef2f209116be737dca5076bc6213d
MainMemory <sonicmike2@yahoo.com>
2014-06-01 21:44:55 -0500

    Removing "Puyo Tools" folder, updating editors to new Puyo Tools.

--- SAModel.Direct3D/TextureSystem/TextureArchive.cs
+++ SAModel.Direct3D/TextureSystem/TextureArchive.cs
@@ -2,76 +2,71 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Windows.Forms;
-
-using PuyoTools;
-
+using PuyoTools.Modules.Archive;
+using VrSharp;
+using VrSharp.GvrTexture;
 using VrSharp.PvrTexture;
 
 namespace SonicRetro.SAModel.Direct3D.TextureSystem
 {
-    /// <summary>
-    /// This TextureArchive class is the primary interface for retrieving a texture list/array from a container format, such as PVM/GVM, and eventually PAK.
-    /// </summary>
-    public static class TextureArchive
-    {
-        public static BMPInfo[] GetTextures(string filename)
-        {
-            List<BMPInfo> functionReturnValue = new List<BMPInfo>();
-            bool gvm = false;
-            ArchiveModule pvmfile = null;
-            Stream pvmdata = new MemoryStream(File.ReadAllBytes(filename));
-            if (Path.GetExtension(filename).Equals(".prs", StringComparison.OrdinalIgnoreCase))
-            {
-                Stream decomp = new MemoryStream();
-                FraGag.Compression.Prs.Decompress(pvmdata, decomp);
-                pvmdata.Dispose();
-                pvmdata = decomp;
-            }
-            pvmfile = new PVM();
-            if (!pvmfile.Check(pvmdata, filename))
-            {
-                pvmfile = new GVM();
-                gvm = true;
-            }
-            VrSharp.VpClut pvp = null;
-            pvmdata = pvmfile.TranslateData(pvmdata);
-            ArchiveFileList pvmentries = pvmfile.GetFileList(pvmdata);
-            foreach (ArchiveFileList.Entry file in pvmentries.Entries)
-            {
-                byte[] data = new byte[file.Length];
-                pvmdata.Seek(file.Offset, SeekOrigin.Begin);
-                pvmdata.Read(data, 0, (int)file.Length);
-                VrSharp.VrTexture vrfile = gvm ? (VrSharp.VrTexture)new VrSharp.GvrTexture.GvrTexture(data) : (VrSharp.VrTexture)new VrSharp.PvrTexture.PvrTexture(data);
-                if (vrfile.NeedsExternalClut())
-                {
-                    using (System.Windows.Forms.OpenFileDialog a = new System.Windows.Forms.OpenFileDialog
-                    {
-                        DefaultExt = gvm ? "gvp" : "pvp",
-                        Filter = gvm ? "GVP Files|*.gvp" : "PVP Files|*.pvp",
-                        InitialDirectory = System.IO.Path.GetDirectoryName(filename),
-                        Title = "External palette file"
-                    })
-                    {
-                        if (pvp == null)
-                            if (a.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                                pvp = gvm ? (VrSharp.VpClut)new VrSharp.GvrTexture.GvpClut(a.FileName) : (VrSharp.VpClut)new PvpClut(a.FileName);
-                            else
-                                return new BMPInfo[0];
-                    }
-                    vrfile.SetClut(pvp);
-                }
-                try
-                {
-                    functionReturnValue.Add(new BMPInfo(Path.GetFileNameWithoutExtension(file.Filename), vrfile.GetTextureAsBitmap()));
-                }
-                catch
-                {
-                    functionReturnValue.Add(new BMPInfo(Path.GetFileNameWithoutExtension(file.Filename), new Bitmap(1, 1)));
-                }
-            }
-            pvmdata.Dispose();
-            return functionReturnValue.ToArray();
-        }
-    }
+	/// <summary>
+	/// This TextureArchive class is the primary interface for retrieving a texture list/array from a container format, such as PVM/GVM, and eventually PAK.
+	/// </summary>
+	public static class TextureArchive
+	{
+		public static BMPInfo[] GetTextures(string filename)
+		{
+			List<BMPInfo> functionReturnValue = new List<BMPInfo>();
+			bool gvm = false;
+			ArchiveBase pvmfile = null;
+			byte[] pvmdata = File.ReadAllBytes(filename);
+			if (Path.GetExtension(filename).Equals(".prs", StringComparison.OrdinalIgnoreCase))
+				pvmdata = FraGag.Compression.Prs.Decompress(pvmdata);
+			pvmfile = new PvmArchive();
+			if (!pvmfile.Is(pvmdata, filename))
+			{
+				pvmfile = new GvmArchive();
+				gvm = true;
+			}
+			VrSharp.VpPalette pvp = null;
+			ArchiveEntryCollection pvmentries = pvmfile.Open(pvmdata).Entries;
+			foreach (ArchiveEntry file in pvmentries)
+			{
+				byte[] data = new byte[file.Length];
+				using (Stream str = file.Open())
+					str.Read(data, 0, file.Length);
+				VrTexture vrfile = gvm ? (VrTexture)new GvrTexture(data) : (VrTexture)new PvrTexture(data);
+				if (vrfile.NeedsExternalPalette)
+				{
+					using (System.Windows.Forms.OpenFileDialog a = new System.Windows.Forms.OpenFileDialog
+					{
+						DefaultExt = gvm ? "gvp" : "pvp",
+						Filter = gvm ? "GVP Files|*.gvp" : "PVP Files|*.pvp",
+						InitialDirectory = System.IO.Path.GetDirectoryName(filename),
+						Title = "External palette file"
+					})
+					{
+						if (pvp == null)
+							if (a.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+								pvp = gvm ? (VpPalette)new GvpPalette(a.FileName) : (VpPalette)new PvpPalette(a.FileName);
+							else
+								return new BMPInfo[0];
+					}
+					if (gvm)
+						((GvrTexture)vrfile).SetPalette((GvpPalette)pvp);
+					else
+						((PvrTexture)vrfile).SetPalette((PvpPalette)pvp);
+				}
+				try
+				{
+					functionReturnValue.Add(new BMPInfo(Path.GetFileNameWithoutExtension(file.Name), vrfile.ToBitmap()));
+				}
+				catch
+				{
+					functionReturnValue.Add(new BMPInfo(Path.GetFileNameWithoutExtension(file.Name), new Bitmap(1, 1)));
+				}
+			}
+			return functionReturnValue.ToArray();
+		}
+	}
 }
