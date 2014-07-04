7c83aadb8a50ab9ae4bfad339ef8678f480fc130
MainMemory <sonicmike2@yahoo.com>
2014-07-04 10:12:05 -0500

    Adding SA2 Stage Select Editor.

---
+++ SA2StageSelEdit/MainForm.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace SA2StageSelEdit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.Windows.Forms.Label label1;
+			System.Windows.Forms.Label label2;
+			System.Windows.Forms.Label label3;
+			System.Windows.Forms.Label label4;
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.row = new System.Windows.Forms.NumericUpDown();
+			this.column = new System.Windows.Forms.NumericUpDown();
+			this.character = new System.Windows.Forms.ComboBox();
+			this.level = new System.Windows.Forms.ComboBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			label1 = new System.Windows.Forms.Label();
+			label2 = new System.Windows.Forms.Label();
+			label3 = new System.Windows.Forms.Label();
+			label4 = new System.Windows.Forms.Label();
+			this.menuStrip1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.row)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.column)).BeginInit();
+			this.panel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new System.Drawing.Point(6, 6);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(36, 13);
+			label1.TabIndex = 0;
+			label1.Text = "Level:";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new System.Drawing.Point(6, 33);
+			label2.Name = "label2";
+			label2.Size = new System.Drawing.Size(56, 13);
+			label2.TabIndex = 0;
+			label2.Text = "Character:";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new System.Drawing.Point(6, 59);
+			label3.Name = "label3";
+			label3.Size = new System.Drawing.Size(45, 13);
+			label3.TabIndex = 0;
+			label3.Text = "Column:";
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new System.Drawing.Point(6, 85);
+			label4.Name = "label4";
+			label4.Size = new System.Drawing.Size(32, 13);
+			label4.TabIndex = 0;
+			label4.Text = "Row:";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+			this.openToolStripMenuItem.Text = "&Open...";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Enabled = false;
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+			this.saveToolStripMenuItem.Text = "&Save";
+			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(109, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+			this.exitToolStripMenuItem.Text = "E&xit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Margin = new System.Windows.Forms.Padding(0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(2048, 480);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+			this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.row);
+			this.panel2.Controls.Add(label4);
+			this.panel2.Controls.Add(label3);
+			this.panel2.Controls.Add(this.column);
+			this.panel2.Controls.Add(label2);
+			this.panel2.Controls.Add(this.character);
+			this.panel2.Controls.Add(this.level);
+			this.panel2.Controls.Add(label1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel2.Enabled = false;
+			this.panel2.Location = new System.Drawing.Point(384, 24);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(200, 538);
+			this.panel2.TabIndex = 1;
+			// 
+			// row
+			// 
+			this.row.Location = new System.Drawing.Point(92, 83);
+			this.row.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+			this.row.Name = "row";
+			this.row.Size = new System.Drawing.Size(96, 20);
+			this.row.TabIndex = 2;
+			this.row.ValueChanged += new System.EventHandler(this.row_ValueChanged);
+			// 
+			// column
+			// 
+			this.column.Location = new System.Drawing.Point(92, 57);
+			this.column.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+			this.column.Name = "column";
+			this.column.Size = new System.Drawing.Size(96, 20);
+			this.column.TabIndex = 0;
+			this.column.ValueChanged += new System.EventHandler(this.column_ValueChanged);
+			// 
+			// character
+			// 
+			this.character.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.character.FormattingEnabled = true;
+			this.character.Items.AddRange(new object[] {
+            "Sonic",
+            "Shadow",
+            "Tails",
+            "Eggman",
+            "Knuckles",
+            "Rouge",
+            "Mech Tails",
+            "Mech Eggman"});
+			this.character.Location = new System.Drawing.Point(68, 30);
+			this.character.Name = "character";
+			this.character.Size = new System.Drawing.Size(120, 21);
+			this.character.TabIndex = 0;
+			this.character.SelectedIndexChanged += new System.EventHandler(this.character_SelectedIndexChanged);
+			// 
+			// level
+			// 
+			this.level.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.level.FormattingEnabled = true;
+			this.level.Items.AddRange(new object[] {
+            "Basic Test",
+            "Knuckles Test",
+            "Sonic Test",
+            "Green Forest",
+            "White Jungle",
+            "Pumpkin Hill",
+            "Sky Rail",
+            "Aquatic Mine",
+            "Security Hall",
+            "Prison Lane",
+            "Metal Harbor",
+            "Iron Gate",
+            "Weapons Bed",
+            "City Escape",
+            "Radical Highway",
+            "Weapons Bed 2P",
+            "Wild Canyon",
+            "Mission Street",
+            "Dry Lagoon",
+            "Sonic Vs. Shadow 1",
+            "Tails Vs. Eggman 1",
+            "Sand Ocean",
+            "Crazy Gadget",
+            "Hidden Base",
+            "Eternal Engine",
+            "Death Chamber",
+            "Egg Quarters",
+            "Lost Colony",
+            "Pyramid Cave",
+            "Tails Vs. Eggman 2",
+            "Final Rush",
+            "Green Hill",
+            "Meteor Herd",
+            "Knuckles Vs. Rouge",
+            "Cannon\'s Core (Sonic)",
+            "Cannon\'s Core (Eggman)",
+            "Cannon\'s Core (Tails)",
+            "Cannon\'s Core (Rouge)",
+            "Cannon\'s Core (Knuckles)",
+            "Mission Street 2P",
+            "Final Chase",
+            "Wild Canyon 2P",
+            "Sonic Vs. Shadow 2",
+            "Cosmic Wall",
+            "Mad Space",
+            "Sand Ocean 2P",
+            "Dry Lagoon 2P",
+            "Pyramid Race",
+            "Hidden Base 2P",
+            "Pool Quest",
+            "Planet Quest",
+            "Deck Race",
+            "Downtown Race",
+            "Cosmic Wall 2P",
+            "Grind Race",
+            "Lost Colony 2P",
+            "Eternal Engine 2P",
+            "Metal Harbor 2P",
+            "Iron Gate 2P",
+            "Death Chamber 2P",
+            "F-6t BIG FOOT",
+            "B-3x HOT SHOT",
+            "R-1/A FLYING DOG",
+            "King Boom Boo",
+            "Egg Golem (Sonic)",
+            "Biolizard",
+            "FinalHazard",
+            "Egg Golem (Eggman)",
+            "68",
+            "69",
+            "Route 101/Route 280",
+            "Kart Race",
+            "72",
+            "73",
+            "74",
+            "75",
+            "76",
+            "77",
+            "78",
+            "79",
+            "80",
+            "81",
+            "82",
+            "83",
+            "84",
+            "85",
+            "86",
+            "87",
+            "88",
+            "89",
+            "Chao World"});
+			this.level.Location = new System.Drawing.Point(48, 3);
+			this.level.Name = "level";
+			this.level.Size = new System.Drawing.Size(140, 21);
+			this.level.TabIndex = 1;
+			this.level.SelectedIndexChanged += new System.EventHandler(this.level_SelectedIndexChanged);
+			// 
+			// panel3
+			// 
+			this.panel3.AutoScroll = true;
+			this.panel3.Controls.Add(this.panel1);
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel3.Location = new System.Drawing.Point(0, 24);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(384, 538);
+			this.panel3.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(584, 562);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "SA2 Stage Select Editor";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.row)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.column)).EndInit();
+			this.panel3.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox level;
+        private System.Windows.Forms.ComboBox character;
+        private System.Windows.Forms.NumericUpDown column;
+        private System.Windows.Forms.NumericUpDown row;
+    }
+}
+
---
+++ SA2StageSelEdit/MainForm.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using PAKLib;
+using SonicRetro.SAModel.Direct3D.TextureSystem;
+using SA_Tools;
+using System.Linq;
+
+namespace SA2StageSelEdit
+{
+    public partial class MainForm : Form
+    {
+        static readonly int chaoicon = 6;
+        static readonly int questionicon = 9;
+        static readonly int selecticon = 15;
+		static readonly Dictionary<SA2Characters, int> charicons = new Dictionary<SA2Characters, int>() {
+			{ SA2Characters.Sonic, 12 },
+			{ SA2Characters.Shadow, 11 },
+			{ SA2Characters.Tails, 13 },
+			{ SA2Characters.Eggman, 7 },
+			{ SA2Characters.Knuckles, 8 },
+			{ SA2Characters.Rouge, 10 },
+			{ SA2Characters.MechTails, 13 },
+			{ SA2Characters.MechEggman, 7 }
+		};
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        Device d3ddevice;
+        Camera cam = new Camera();
+        static readonly CustomVertex.PositionTextured[] SquareVerts = {
+        new CustomVertex.PositionTextured(0, 48, 0, 0, 1),
+        new CustomVertex.PositionTextured(0, 0, 0, 0, 0),
+        new CustomVertex.PositionTextured(48, 48, 0, 1, 1),
+        new CustomVertex.PositionTextured(0, 0, 0, 0, 0),
+        new CustomVertex.PositionTextured(48, 0, 0, 1, 0),
+        new CustomVertex.PositionTextured(48, 48, 0, 1, 1)};
+        static Microsoft.DirectX.Direct3D.Mesh SquareMesh;
+        string filename;
+        List<StageSelectLevel> levels;
+        Texture bgtex;
+        Texture[] uitexs;
+        int selected;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.Opaque, true);
+            d3ddevice = new Device(0, DeviceType.Hardware, panel1.Handle, CreateFlags.SoftwareVertexProcessing, new PresentParameters[] { new PresentParameters() { Windowed = true, SwapEffect = SwapEffect.Discard, EnableAutoDepthStencil = true, AutoDepthStencilFormat = DepthFormat.D24X8 } });
+            d3ddevice.Lights[0].Type = LightType.Directional;
+            d3ddevice.Lights[0].Diffuse = Color.White;
+            d3ddevice.Lights[0].Ambient = Color.White;
+            d3ddevice.Lights[0].Specular = Color.White;
+            d3ddevice.Lights[0].Range = 100000;
+            d3ddevice.Lights[0].Direction = Vector3.Normalize(new Vector3(0, -1, 0));
+            d3ddevice.Lights[0].Enabled = true;
+            SquareMesh = new Mesh(2, 6, MeshFlags.Managed, CustomVertex.PositionTextured.Format, d3ddevice);
+            List<short> ib = new List<short>();
+            for (int i = 0; i < SquareVerts.Length; i++)
+                ib.Add((short)(i));
+            SquareMesh.SetVertexBufferData(SquareVerts, LockFlags.None);
+            SquareMesh.SetIndexBufferData(ib.ToArray(), LockFlags.None);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fd = new OpenFileDialog() { DefaultExt = "ini", Filter = "INI Files|*.ini" })
+                if (fd.ShowDialog(this) == DialogResult.OK)
+                {
+					IniData ini = IniSerializer.Deserialize<IniData>(fd.FileName);
+					filename = Path.Combine(Path.GetDirectoryName(fd.FileName), 
+						ini.Files.First((item) => item.Value.Type == "stageselectlist").Value.Filename);
+					levels = new List<StageSelectLevel>(StageSelectLevelList.Load(filename));
+                    string resdir = Path.Combine(Path.GetDirectoryName(fd.FileName), ini.SystemFolder);
+                    using (Stream str = new MemoryStream(
+                        new PAKFile(Path.Combine(resdir, @"SOC\stageMapBG.pak")).Files.Find(
+                        (a) => a.Name.Equals(@"stagemapbg\stagemap.dds")).Data))
+                        bgtex = TextureLoader.FromStream(d3ddevice, str);
+                    if (File.Exists(Path.Combine(resdir, @"PRS\stageMap.pak")))
+                    {
+                        List<PAKFile.File> files = new PAKFile(Path.Combine(resdir, @"PRS\stageMap.pak")).Files;
+                        byte[] inf = files.Find((a) => a.Name.Equals(@"stagemap\stagemap.inf")).Data;
+                        uitexs = new Texture[inf.Length / 0x3C];
+                        for (int i = 0; i < uitexs.Length; i++)
+                            using (Stream str = new MemoryStream(files.Find(
+                                (a) => a.Name.Equals(@"stagemap\" + Encoding.ASCII.GetString(inf, i * 0x3C, 0x1c).TrimEnd('\0') + ".dds")).Data))
+                                uitexs[i] = TextureLoader.FromStream(d3ddevice, str);
+                    }
+                    else
+                    {
+						BMPInfo[] bmps = TextureArchive.GetTextures(Path.Combine(resdir, "stageMap.prs"));
+                        uitexs = new Texture[bmps.Length];
+						for (int i = 0; i < bmps.Length; i++)
+							uitexs[i] = new Texture(d3ddevice, bmps[i].Image, Usage.SoftwareProcessing, Pool.Managed);
+                    }
+                    saveToolStripMenuItem.Enabled = panel1.Enabled = panel2.Enabled = true;
+					selected = 0;
+                    level.SelectedIndex = (int)levels[selected].Level;
+                    character.SelectedIndex = (int)levels[selected].Character;
+                    column.Value = levels[selected].Column;
+                    row.Value = levels[selected].Row;
+                    DrawLevel();
+                }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			levels.ToArray().Save(filename);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) { DrawLevel(); }
+
+        void DrawLevel()
+        {
+            if (string.IsNullOrEmpty(filename)) return;
+            d3ddevice.SetTransform(TransformType.Projection, Matrix.OrthoOffCenterRH(0, panel1.Width, panel1.Height, 0, 1, 10000));
+            d3ddevice.SetTransform(TransformType.View, cam.ToMatrix());
+            d3ddevice.Transform.World = Matrix.Identity;
+            d3ddevice.Material = new Material() { Ambient = Color.White, Diffuse = Color.White };
+            d3ddevice.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Magenta.ToArgb(), 1, 0);
+            d3ddevice.RenderState.ZBufferEnable = true;
+            d3ddevice.VertexFormat = CustomVertex.PositionTextured.Format;
+            d3ddevice.BeginScene();
+            //all drawings after this line
+            d3ddevice.SetSamplerState(0, SamplerStageStates.MinFilter, (int)TextureFilter.Anisotropic);
+            d3ddevice.SetSamplerState(0, SamplerStageStates.MagFilter, (int)TextureFilter.Anisotropic);
+            d3ddevice.SetSamplerState(0, SamplerStageStates.MipFilter, (int)TextureFilter.Anisotropic);
+            d3ddevice.SetRenderState(RenderStates.Lighting, true);
+            d3ddevice.SetRenderState(RenderStates.SpecularEnable, false);
+            d3ddevice.SetRenderState(RenderStates.Ambient, Color.White.ToArgb());
+            d3ddevice.SetRenderState(RenderStates.AlphaBlendEnable, true);
+            d3ddevice.SetRenderState(RenderStates.BlendOperation, (int)BlendOperation.Add);
+            d3ddevice.SetRenderState(RenderStates.DestinationBlend, (int)Blend.InvSourceAlpha);
+            d3ddevice.SetRenderState(RenderStates.SourceBlend, (int)Blend.SourceAlpha);
+            d3ddevice.SetRenderState(RenderStates.AlphaTestEnable, true);
+            d3ddevice.SetRenderState(RenderStates.AlphaFunction, (int)Compare.Greater);
+            d3ddevice.SetRenderState(RenderStates.AmbientMaterialSource, (int)ColorSource.Material);
+            d3ddevice.SetRenderState(RenderStates.DiffuseMaterialSource, (int)ColorSource.Material);
+            d3ddevice.SetRenderState(RenderStates.SpecularMaterialSource, (int)ColorSource.Material);
+            d3ddevice.SetTextureStageState(0, TextureStageStates.AlphaOperation, (int)TextureOperation.BlendDiffuseAlpha);
+            using (Sprite spr = new Sprite(d3ddevice))
+            {
+                spr.Begin(SpriteFlags.None);
+                spr.Draw2D(bgtex, PointF.Empty, 0, PointF.Empty, Color.White);
+                spr.Flush();
+                spr.End();
+            }
+            d3ddevice.SetTexture(0, bgtex);
+            SquareMesh.DrawSubset(0);
+            foreach (StageSelectLevel item in levels)
+            {
+                int tex = 0;
+                switch (item.Level)
+                {
+                    case SA2LevelIDs.GreenHill: // Green Hill
+                        tex = questionicon;
+                        break;
+                    case SA2LevelIDs.ChaoWorld: // Chao World
+                        tex = chaoicon;
+                        break;
+                    default:
+                        tex = charicons[item.Character];
+                        break;
+                }
+                d3ddevice.Transform.World = Matrix.Translation(item.Column * 60 + 110, item.Row * 60 + 10, -1);
+                d3ddevice.SetTexture(0, uitexs[tex]);
+                SquareMesh.DrawSubset(0);
+            }
+            d3ddevice.Transform.World = Matrix.Scaling(2, 2, 1) * Matrix.Translation(levels[selected].Column * 60 + 104, levels[selected].Row * 60 + 4, -1);
+            d3ddevice.SetTexture(0, uitexs[selecticon]);
+            SquareMesh.DrawSubset(0);
+            d3ddevice.EndScene(); //all drawings before this line
+            d3ddevice.Present();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (string.IsNullOrEmpty(filename)) return;
+            bool hit = false;
+            foreach (StageSelectLevel item in levels)
+                if (new Rectangle(item.Column * 60 + 110, item.Row * 60 + 10, 48, 48).Contains(e.Location))
+                    hit = true;
+            panel1.Cursor = hit ? Cursors.Hand : Cursors.Default;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (string.IsNullOrEmpty(filename)) return;
+            foreach (StageSelectLevel item in levels)
+                if (new Rectangle(item.Column * 60 + 110, item.Row * 60 + 10, 48, 48).Contains(e.Location))
+                {
+                    selected = levels.IndexOf(item);
+                    DrawLevel();
+                    level.SelectedIndex = (int)item.Level;
+                    character.SelectedIndex = (int)item.Character;
+                    column.Value = item.Column;
+                    row.Value = item.Row;
+                    return;
+                }
+        }
+
+        private void level_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            levels[selected].Level = (SA2LevelIDs)level.SelectedIndex;
+            DrawLevel();
+        }
+
+        private void character_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            levels[selected].Character = (SA2Characters)character.SelectedIndex;
+            DrawLevel();
+        }
+
+        private void column_ValueChanged(object sender, EventArgs e)
+        {
+            levels[selected].Column = (int)column.Value;
+            DrawLevel();
+        }
+
+        private void row_ValueChanged(object sender, EventArgs e)
+        {
+            levels[selected].Row = (int)row.Value;
+            DrawLevel();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            switch (MessageBox.Show(this, "Do you want to save?", Text, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question))
+            {
+                case DialogResult.Cancel:
+                    e.Cancel = true;
+                    break;
+                case DialogResult.Yes:
+                    saveToolStripMenuItem_Click(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+
+    public class Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Up { get; private set; }
+        public Vector3 Look { get; private set; }
+        public Vector3 Right { get; private set; }
+
+        public Camera() { }
+
+        public Matrix ToMatrix()
+        {
+            Up = new Vector3(0, 1, 0);
+            Look = new Vector3(0, 0, 1);
+            Right = new Vector3(1, 0, 0);
+            Matrix ViewMatrix = Matrix.Identity;
+            ViewMatrix.M11 = Right.X;
+            ViewMatrix.M12 = Up.X;
+            ViewMatrix.M13 = Look.X;
+            ViewMatrix.M21 = Right.Y;
+            ViewMatrix.M22 = Up.Y;
+            ViewMatrix.M23 = Look.Y;
+            ViewMatrix.M31 = Right.Z;
+            ViewMatrix.M32 = Up.Z;
+            ViewMatrix.M33 = Look.Z;
+            ViewMatrix.M41 = -Vector3.Dot(Position, Right);
+            ViewMatrix.M42 = -Vector3.Dot(Position, Up);
+            ViewMatrix.M43 = -Vector3.Dot(Position, Look);
+            return ViewMatrix;
+        }
+    }
+}
