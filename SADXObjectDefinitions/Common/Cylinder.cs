9ea955a761f149689dbbab91ea2443f6de4dd655
SonicFreak94 <sonicfreak94@hotmail.com>
2014-11-10 23:08:16 -0700

    SADXObjectDefinitions: Fixed Cylnder scaling wildly out of control.

--- SADXObjectDefinitions/Common/Cylinder.cs
+++ SADXObjectDefinitions/Common/Cylinder.cs
@@ -9,41 +9,51 @@ using SonicRetro.SAModel.SAEditorCommon.DataTypes;
 
 namespace SADXObjectDefinitions.Common
 {
-    public class Cylinder : ObjectDefinition
-    {
-        private SonicRetro.SAModel.Object model;
-        private Microsoft.DirectX.Direct3D.Mesh[] meshes;
+	public class Cylinder : ObjectDefinition
+	{
+		private SonicRetro.SAModel.Object model;
+		private Microsoft.DirectX.Direct3D.Mesh[] meshes;
 
-        public override void Init(ObjectData data, string name, Device dev)
-        {
-            model = ObjectHelper.LoadModel("Objects/Collision/Cylinder Model.sa1mdl");
-            meshes = ObjectHelper.GetMeshes(model, dev);
-        }
+		public override void Init(ObjectData data, string name, Device dev)
+		{
+			model = ObjectHelper.LoadModel("Objects/Collision/Cylinder Model.sa1mdl");
+			meshes = ObjectHelper.GetMeshes(model, dev);
+		}
 
-        public override HitResult CheckHit(SETItem item, Vector3 Near, Vector3 Far, Viewport Viewport, Matrix Projection, Matrix View, MatrixStack transform)
-        {
-            transform.Push();
-            transform.NJTranslate(item.Position);
-            transform.NJRotateY(item.Rotation.Y);
-            transform.NJScale((item.Scale.X + 5) / 10f, (item.Scale.Y + 5) / 10f, (item.Scale.Z + 5) / 10f);
-            HitResult result = model.CheckHit(Near, Far, Viewport, Projection, View, transform, meshes);
-            transform.Pop();
-            return result;
-        }
+		public override HitResult CheckHit(SETItem item, Vector3 Near, Vector3 Far, Viewport Viewport, Matrix Projection, Matrix View, MatrixStack transform)
+		{
+			transform.Push();
+			transform.NJTranslate(item.Position);
+			transform.NJRotateY(item.Rotation.Y);
+
+			float X, Y;
+			X = (float)((item.Scale.X + 10.0) * 0.1f);
+			Y = (float)((item.Scale.Y + 10.0) * 0.1f);
+			transform.NJScale(X, Y, X);
+
+			HitResult result = model.CheckHit(Near, Far, Viewport, Projection, View, transform, meshes);
+			transform.Pop();
+			return result;
+		}
 
 		public override RenderInfo[] Render(SETItem item, Device dev, EditorCamera camera, MatrixStack transform, bool selected)
-        {
-            List<RenderInfo> result = new List<RenderInfo>();
-            transform.Push();
-            transform.NJTranslate(item.Position);
-            transform.NJRotateY(item.Rotation.Y);
-            transform.NJScale((item.Scale.X + 5) / 10f, (item.Scale.Y + 5) / 10f, (item.Scale.Z + 5) / 10f);
-            result.AddRange(model.DrawModelTree(dev, transform, null, meshes));
-            if (selected)
-                result.AddRange(model.DrawModelTreeInvert(dev, transform, meshes));
-            transform.Pop();
-            return result.ToArray();
-        }
+		{
+			List<RenderInfo> result = new List<RenderInfo>();
+			transform.Push();
+			transform.NJTranslate(item.Position);
+			transform.NJRotateY(item.Rotation.Y);
+
+			float X, Y;
+			X = (float)((item.Scale.X + 10.0) * 0.1f);
+			Y = (float)((item.Scale.Y + 10.0) * 0.1f);
+			transform.NJScale(X, Y, X);
+
+			result.AddRange(model.DrawModelTree(dev, transform, null, meshes));
+			if (selected)
+				result.AddRange(model.DrawModelTreeInvert(dev, transform, meshes));
+			transform.Pop();
+			return result.ToArray();
+		}
 
 		public override SonicRetro.SAModel.BoundingSphere GetBounds(SETItem item)
 		{
@@ -56,6 +66,6 @@ namespace SADXObjectDefinitions.Common
 			return boxSphere;
 		}
 
-        public override string Name { get { return "Solid Cylinder"; } }
-    }
+		public override string Name { get { return "Solid Cylinder"; } }
+	}
 }
