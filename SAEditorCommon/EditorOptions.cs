06839db7d2b7d724bca7c51cdf8bedb5a6ddf511
SonicFreak94 <sonicfreak94@hotmail.com>
2015-04-09 03:02:04 -0600

    SAEditorCommon: Fixed fullbright lighting.

--- SAEditorCommon/EditorOptions.cs
+++ SAEditorCommon/EditorOptions.cs
@@ -87,7 +87,7 @@ namespace SonicRetro.SAModel.SAEditorCommon
             d3ddevice.SamplerState[0].MinFilter = TextureFilter.Anisotropic;
 			d3ddevice.SamplerState[0].MagFilter = TextureFilter.Anisotropic;
 			d3ddevice.SamplerState[0].MipFilter = TextureFilter.Anisotropic;
-			d3ddevice.RenderState.Lighting = true;
+			d3ddevice.RenderState.Lighting = !overrideLighting;
 			d3ddevice.RenderState.SpecularEnable = true;
             if (!OverrideLighting) d3ddevice.RenderState.Ambient = Color.Black;
             else d3ddevice.RenderState.Ambient = Color.White;
