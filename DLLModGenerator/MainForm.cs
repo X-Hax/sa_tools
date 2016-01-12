e6f8e2ad24663a4f67dae0e58bed23763fd1d5c0
MainMemory <sonicmike2@yahoo.com>
2016-01-12 17:49:30 -0600

    Apparently I don't want to call that function.

--- DLLModGenerator/MainForm.cs
+++ DLLModGenerator/MainForm.cs
@@ -80,7 +80,7 @@ namespace DLLModGenerator
 			recentProjectsToolStripMenuItem.DropDownItems.Insert(0, new ToolStripMenuItem(filename));
 			Environment.CurrentDirectory = Path.GetDirectoryName(filename);
 			listView1.BeginUpdate();
-			listView1.Clear();
+			listView1.Items.Clear();
 			foreach (KeyValuePair<string, FileTypeHash> item in IniData.Files)
 			{
 				bool modified = HelperFunctions.FileHash(item.Key) != item.Value.Hash;
