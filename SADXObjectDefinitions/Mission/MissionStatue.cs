3a0b9e4544bcb555624f5de420722185679781f4
MainMemory <mainmemory@yahoo.com>
2017-01-29 14:43:54 -0600

    Adding mission timer and mission statue object definitions.

---
+++ SADXObjectDefinitions/Mission/MissionStatue.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using SonicRetro.SAModel;
+using SonicRetro.SAModel.Direct3D;
+using SonicRetro.SAModel.SAEditorCommon.DataTypes;
+using SonicRetro.SAModel.SAEditorCommon.SETEditing;
+using System.Collections.Generic;
+
+namespace SADXObjectDefinitions.Mission
+{
+	class MissionStatue : ObjectDefinition
+	{
+		private NJS_OBJECT model;
+		private Mesh[] meshes;
+
+		public override void Init(ObjectData data, string name, Device dev)
+		{
+			model = ObjectHelper.LoadModel("Objects/Mission/Mission Statue.sa1mdl");
+			meshes = ObjectHelper.GetMeshes(model, dev);
+		}
+
+		public override HitResult CheckHit(SETItem item, Vector3 Near, Vector3 Far, Viewport Viewport, Matrix Projection, Matrix View, MatrixStack transform)
+		{
+			transform.Push();
+			transform.NJTranslate(item.Position.X, item.Position.Y + 8, item.Position.Z);
+			transform.NJRotateY(item.Rotation.Y);
+			HitResult result = model.CheckHit(Near, Far, Viewport, Projection, View, transform, meshes);
+			transform.Pop();
+			return result;
+		}
+
+		public override List<RenderInfo> Render(SETItem item, Device dev, EditorCamera camera, MatrixStack transform)
+		{
+			List<RenderInfo> result = new List<RenderInfo>();
+			transform.Push();
+			transform.NJTranslate(item.Position.X, item.Position.Y + 8, item.Position.Z);
+			transform.NJRotateY(item.Rotation.Y);
+			result.AddRange(model.DrawModelTree(dev, transform, ObjectHelper.GetTextures("mi_3dasu"), meshes));
+			if (item.Selected)
+				result.AddRange(model.DrawModelTreeInvert(dev, transform, meshes));
+			transform.Pop();
+			return result;
+		}
+
+		public override string Name { get { return "Mission Statue"; } }
+	}
+}
---
+++ SADXObjectDefinitions/Mission/MissionTimer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using SonicRetro.SAModel.Direct3D;
+using SonicRetro.SAModel.SAEditorCommon.DataTypes;
+using SonicRetro.SAModel.SAEditorCommon.SETEditing;
+using System.Collections.Generic;
+
+namespace SADXObjectDefinitions.Mission
+{
+	class MissionTimer : ObjectDefinition
+	{
+		public override void Init(ObjectData data, string name, Device dev) { }
+
+		public override HitResult CheckHit(SETItem item, Vector3 Near, Vector3 Far, Viewport Viewport, Matrix Projection, Matrix View, MatrixStack transform)
+		{
+			transform.Push();
+			transform.NJTranslate(item.Position);
+			HitResult result = ObjectHelper.CheckSpriteHit(Near, Far, Viewport, Projection, View, transform);
+			transform.Pop();
+			return result;
+		}
+
+		public override List<RenderInfo> Render(SETItem item, Device dev, EditorCamera camera, MatrixStack transform)
+		{
+			List<RenderInfo> result = new List<RenderInfo>();
+			transform.Push();
+			transform.NJTranslate(item.Position);
+			result.AddRange(ObjectHelper.RenderSprite(dev, transform, null, item.Position.ToVector3(), item.Selected));
+			transform.Pop();
+			return result;
+		}
+
+		public override string Name { get { return "Mission Timer"; } }
+
+		private PropertySpec[] customProperties = new PropertySpec[] {
+			new PropertySpec("Switch ID", typeof(byte), null, null, true, (o) => ((MissionSETItem)o).PRMBytes[4], (o, v) => ((MissionSETItem)o).PRMBytes[4] = (byte)v),
+			new PropertySpec("Minutes", typeof(byte), null, null, true, (o) => ((MissionSETItem)o).PRMBytes[5], (o, v) => ((MissionSETItem)o).PRMBytes[5] = (byte)v),
+			new PropertySpec("Seconds", typeof(byte), null, null, true, (o) => ((MissionSETItem)o).PRMBytes[6], (o, v) => ((MissionSETItem)o).PRMBytes[6] = (byte)v),
+			new PropertySpec("Frames", typeof(byte), null, null, true, (o) => ((MissionSETItem)o).PRMBytes[7], (o, v) => ((MissionSETItem)o).PRMBytes[7] = (byte)v)
+		};
+
+		public override PropertySpec[] CustomProperties { get { return customProperties; } }
+	}
+}
