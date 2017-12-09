7d2d55102f1b6c8666416b2bfc4e74b1c69e1e53
Michael Fadely <michaelwfadely@gmail.com>
2017-12-08 23:14:01 -0700

    also assembly info

--- VariableListGenerator/Properties/AssemblyInfo.cs
+++ VariableListGenerator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VaraibleListGenerator")]
+[assembly: AssemblyTitle("VariableListGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VaraibleListGenerator")]
+[assembly: AssemblyProduct("VariableListGenerator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
