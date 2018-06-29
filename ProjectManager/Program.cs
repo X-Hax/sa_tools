29cc0b3bc7d7e8edf5a40fcdf4b1e14cacd0113d
Joshua Corvinus <dude.dudeness@gmail.com>
2018-06-28 22:12:25 -0400

    Moving game path validity checking to editor common so that any tool can use it.

--- ProjectManager/Program.cs
+++ ProjectManager/Program.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Windows.Forms;
 using SA_Tools;
 using SonicRetro.SAModel;
-
+using SonicRetro.SAModel.SAEditorCommon;
 
 namespace ProjectManager
 {
@@ -36,102 +36,15 @@ namespace ProjectManager
             //Console.WriteLine("Argument #3 must be an absolute path.");
         }
 
-        public static bool CheckSADXPCValid(out string failReason)
-        {
-            bool sadxPCIsValid = true;
-
-            try // get cast exceptions here
-            {
-                string sadxPCPath = settings.SADXPCPath;
-
-                bool sadxPCPathStringExists = sadxPCPath != null && sadxPCPath.Length > 0;
-                bool sadxPCPathExists = Directory.Exists(sadxPCPath);
-                bool sonicExeExists = File.Exists(string.Concat(sadxPCPath + "\\", "sonic.exe")); // todo: maybe md5 check this so that we can tell it's the right version?
-                bool modLoaderPresent = File.Exists(string.Concat(sadxPCPath + "\\", "SADXModManager.exe"));
-
-                if(!sadxPCPathStringExists)
-                {
-                    failReason = "SADX PC path string did not exist in settings";
-                    return false;
-                }
-                else if (!sadxPCPathExists)
-                {
-                    failReason = string.Format("SADX PC path {0} did not exist or was invalid.", sadxPCPath);
-                    return false;
-                }
-                else if (!sonicExeExists)
-                {
-                    failReason = "SADX PC path did not contain sonic.exe";
-                    return false;
-                }
-                else if (!modLoaderPresent)
-                {
-                    failReason = "SADX PC path did not contain mod loader";
-                    return false;
-                }
-            }
-            catch (System.InvalidCastException e)
-            {
-                failReason = "sadxpc path settings variable was not string type";
-                sadxPCIsValid = false;
-            }
-
-            failReason = "";
-            return sadxPCIsValid;
-        }
-
-        public static bool CheckSA2PCValid(out string failReason)
-        {
-            bool sa2PCIsValid = true;
-
-            try // get cast exceptions here
-            {
-                string sa2PCPath = settings.SA2PCPath;
-
-                bool sa2PCPathStringExists = sa2PCPath != null && sa2PCPath.Length > 0;
-                bool sa2PCPathExists = Directory.Exists(sa2PCPath);
-                bool sonic2ExeExists = File.Exists(string.Concat(sa2PCPath + "\\", "sonic2app.exe"));
-                bool modLoaderPresent = File.Exists(string.Concat(sa2PCPath + "\\", "SA2ModManager.exe"));
-
-                if (!sa2PCPathStringExists)
-                {
-                    failReason = "SA2 PC path string did not exist in settings";
-                    return false;
-                }
-                else if (!sa2PCPathExists)
-                {
-                    failReason = string.Format("SA2 PC path {0} did not exist or was invalid.", sa2PCPath);
-                    return false;
-                }
-                else if (!sonic2ExeExists)
-                {
-                    failReason = "SA2 PC path did not contain sonic2app.exe";
-                    return false;
-                }
-                else if (!modLoaderPresent)
-                {
-                    failReason = "SA2 PC path did not contain mod loader";
-                    return false;
-                }
-            }
-            catch(System.InvalidCastException e)
-            {
-                sa2PCIsValid = false;
-            }
-
-            failReason = "";
-            return sa2PCIsValid;
-        }
-
         public static bool AnyGamesConfigured()
         {
             // sadx validity check
             string sadxpcfailreason = "";
-            bool sadxPCIsValid = CheckSADXPCValid(out sadxpcfailreason);
+            bool sadxPCIsValid = GamePathChecker.CheckSADXPCValid(settings.SADXPCPath, out sadxpcfailreason);
 
             // sa2 valididty check next
             string sa2pcFailReason = "";
-            bool sa2PCIsValid = CheckSA2PCValid(out sa2pcFailReason);
+            bool sa2PCIsValid = GamePathChecker.CheckSA2PCValid(settings.SA2PCPath, out sa2pcFailReason);
 
             return sadxPCIsValid || sa2PCIsValid;
         }
---
+++ SAEditorCommon/GamePathChecker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SonicRetro.SAModel.SAEditorCommon
+{
+    public static class GamePathChecker
+    {
+        public static bool CheckSADXPCValid(string sadxPCPath, out string failReason)
+        {
+            bool sadxPCIsValid = true;
+
+            try // get cast exceptions here
+            {
+                bool sadxPCPathStringExists = sadxPCPath != null && sadxPCPath.Length > 0;
+                bool sadxPCPathExists = Directory.Exists(sadxPCPath);
+                bool sonicExeExists = File.Exists(string.Concat(sadxPCPath + "\\", "sonic.exe")); // todo: maybe md5 check this so that we can tell it's the right version?
+                bool modLoaderPresent = File.Exists(string.Concat(sadxPCPath + "\\", "SADXModManager.exe"));
+
+                if (!sadxPCPathStringExists)
+                {
+                    failReason = "SADX PC path string did not exist in settings";
+                    return false;
+                }
+                else if (!sadxPCPathExists)
+                {
+                    failReason = string.Format("SADX PC path {0} did not exist or was invalid.", sadxPCPath);
+                    return false;
+                }
+                else if (!sonicExeExists)
+                {
+                    failReason = "SADX PC path did not contain sonic.exe";
+                    return false;
+                }
+                else if (!modLoaderPresent)
+                {
+                    failReason = "SADX PC path did not contain mod loader";
+                    return false;
+                }
+            }
+            catch (System.InvalidCastException e)
+            {
+                failReason = "sadxpc path settings variable was not string type";
+                sadxPCIsValid = false;
+            }
+
+            failReason = "";
+            return sadxPCIsValid;
+        }
+
+        public static bool CheckSA2PCValid(string sa2PCPath, out string failReason)
+        {
+            bool sa2PCIsValid = true;
+
+            try // get cast exceptions here
+            {
+                bool sa2PCPathStringExists = sa2PCPath != null && sa2PCPath.Length > 0;
+                bool sa2PCPathExists = Directory.Exists(sa2PCPath);
+                bool sonic2ExeExists = File.Exists(string.Concat(sa2PCPath + "\\", "sonic2app.exe"));
+                bool modLoaderPresent = File.Exists(string.Concat(sa2PCPath + "\\", "SA2ModManager.exe"));
+
+                if (!sa2PCPathStringExists)
+                {
+                    failReason = "SA2 PC path string did not exist in settings";
+                    return false;
+                }
+                else if (!sa2PCPathExists)
+                {
+                    failReason = string.Format("SA2 PC path {0} did not exist or was invalid.", sa2PCPath);
+                    return false;
+                }
+                else if (!sonic2ExeExists)
+                {
+                    failReason = "SA2 PC path did not contain sonic2app.exe";
+                    return false;
+                }
+                else if (!modLoaderPresent)
+                {
+                    failReason = "SA2 PC path did not contain mod loader";
+                    return false;
+                }
+            }
+            catch (System.InvalidCastException e)
+            {
+                sa2PCIsValid = false;
+            }
+
+            failReason = "";
+            return sa2PCIsValid;
+        }
+    }
+}
