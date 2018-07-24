73915f63e1e6d5575de3818c6bf69e66ced66d8a
Joshua Corvinus <dude.dudeness@gmail.com>
2018-07-24 13:54:04 -0400

    Adding vector plane projection

--- SAModel.Direct3D/Extensions.cs
+++ SAModel.Direct3D/Extensions.cs
@@ -21,7 +21,45 @@ namespace SonicRetro.SAModel.Direct3D
 			return new Vector3(rotation.XDeg, rotation.YDeg, rotation.ZDeg);
 		}
 
-		public static Color ToColor(this Vertex vert)
+        #region Project Point On Plane
+        // acquired from here: https://stackoverflow.com/questions/28653628/getting-closest-point-on-a-plane
+        public static Vector3 ProjectPointOnPlane(Vector3 planeNormal, Vector3 planePoint, Vector3 point)
+        {
+            float distance;
+            Vector3 translationVector;
+
+            //First calculate the distance from the point to the plane:
+            distance = SignedDistancePlanePoint(planeNormal, planePoint, point);
+
+            //Reverse the sign of the distance
+            distance *= -1;
+
+            //Get a translation vector
+            translationVector = SetVectorLength(planeNormal, distance);
+
+            //Translate the point to form a projection
+            return point + translationVector;
+        }
+
+        //Get the shortest distance between a point and a plane. The output is signed so it holds information
+        //as to which side of the plane normal the point is.
+        public static float SignedDistancePlanePoint(Vector3 planeNormal, Vector3 planePoint, Vector3 point)
+        {
+            return Vector3.Dot(planeNormal, (point - planePoint));
+        }
+
+        //create a vector of direction "vector" with length "size"
+        public static Vector3 SetVectorLength(Vector3 vector, float size)
+        {
+            //normalize the vector
+            Vector3 vectorNormalized = Vector3.Normalize(vector);
+
+            //scale the vector
+            return vectorNormalized *= size;
+        }
+        #endregion
+
+        public static Color ToColor(this Vertex vert)
 		{
 			return Color.FromArgb(255, (int)(vert.X * 255), (int)(vert.Y * 255), (int)(vert.Z * 255));
 		}
