db33b510f9786e5f97cab8bfcfd9272e087fe171
Joshua Corvinus <dude.dudeness@gmail.com>
2018-08-27 19:20:10 -0400

    Refactoring light code.

--- SAEditorCommon/EditorOptions.cs
+++ SAEditorCommon/EditorOptions.cs
@@ -33,91 +33,59 @@ namespace SonicRetro.SAModel.SAEditorCommon
 			direct3DDevice = d3dDevice;
 
             SetDefaultLights(d3dDevice, false);
-			#region Key Light
-			Light l0 = new Light()
-			{
-				Type = LightType.Directional,
-				Diffuse = Color.FromArgb(255, 180, 172, 172).ToRawColor4(),
-				Ambient = new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1),
-				Specular = new SharpDX.Mathematics.Interop.RawColor4(1, 1, 1, 1),
-				Range = 0,
-				Direction = Vector3.Normalize(new Vector3(-0.245f, -1, 0.125f))
-			};
-			d3dDevice.SetLight(0, ref l0);
-			d3dDevice.EnableLight(0, true);
-			#endregion
-
-			#region Fill Light
-			Light l1 = new Light()
-			{
-				Type = LightType.Directional,
-				Diffuse = Color.FromArgb(255, 132, 132, 132).ToRawColor4(),
-				Ambient = new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1),
-				Specular = new SharpDX.Mathematics.Interop.RawColor4(0.5f, 0.5f, 0.5f, 1),
-				Range = 0,
-				Direction = Vector3.Normalize(new Vector3(0.245f, -0.4f, -0.125f))
-			};
-			d3dDevice.SetLight(1, ref l1);
-			d3dDevice.EnableLight(1, true);
-			#endregion
-
-			#region Back Light
-			Light l2 = new Light()
-			{
-				Type = LightType.Directional,
-				Diffuse = Color.FromArgb(255, 130, 142, 130).ToRawColor4(),
-				Ambient = new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1),
-				Specular = new SharpDX.Mathematics.Interop.RawColor4(0.5f, 0.5f, 0.5f, 1),
-				Range = 0,
-				Direction = Vector3.Normalize(new Vector3(-0.45f, 1f, 0.25f))
-			};
-			d3dDevice.SetLight(2, ref l2);
-			d3dDevice.EnableLight(2, true);
-			#endregion
-
-			#region Font Setup
-			onscreenFont = new Font(d3dDevice, 14, 14, FontWeight.DoNotCare, 0, false, FontCharacterSet.Oem, FontPrecision.Default, FontQuality.Default, FontPitchAndFamily.DontCare, "Verdana");
-			#endregion
 		}
 
         public static void SetDefaultLights(Device d3dDevice, bool reset)
         {
-            foreach(Light light in d3dDevice.Lights)
+            for (int i = 0; i < 4; i++)
             {
-                light.Enabled = false;
+                d3dDevice.EnableLight(i, false);
             }
 
             #region Key Light
-            d3dDevice.Lights[0].Type = LightType.Directional;
-            d3dDevice.Lights[0].DiffuseColor = new ColorValue(180, 172, 172, 255);
-            d3dDevice.Lights[0].Diffuse = Color.FromArgb(255, 180, 172, 172);
-            d3dDevice.Lights[0].Ambient = Color.Black;
-            d3dDevice.Lights[0].Specular = Color.White;
-            d3dDevice.Lights[0].Range = 0;
-            d3dDevice.Lights[0].Direction = Vector3.Normalize(new Vector3(-0.245f, -1, 0.125f));
-            d3dDevice.Lights[0].Enabled = true;
+            Light l0 = new Light()
+            {
+                Type = LightType.Directional,
+                Diffuse = Color.FromArgb(255, 180, 172, 172).ToRawColor4(),
+                Ambient = new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1),
+                Specular = new SharpDX.Mathematics.Interop.RawColor4(1, 1, 1, 1),
+                Range = 0,
+                Direction = Vector3.Normalize(new Vector3(-0.245f, -1, 0.125f))
+            };
+            d3dDevice.SetLight(0, ref l0);
+            d3dDevice.EnableLight(0, true);
             #endregion
 
             #region Fill Light
-            d3dDevice.Lights[1].Type = LightType.Directional;
-            d3dDevice.Lights[1].DiffuseColor = new ColorValue(132, 132, 132, 255);
-            d3dDevice.Lights[1].Diffuse = Color.FromArgb(255, 132, 132, 132);
-            d3dDevice.Lights[1].Ambient = (reset) ? Color.Black : Color.Gray;
-            d3dDevice.Lights[1].Specular = Color.Gray;
-            d3dDevice.Lights[1].Range = 0;
-            d3dDevice.Lights[1].Direction = Vector3.Normalize(new Vector3(0.245f, -0.4f, -0.125f));
-            d3dDevice.Lights[1].Enabled = true;
+            Light l1 = new Light()
+            {
+                Type = LightType.Directional,
+                Diffuse = Color.FromArgb(255, 132, 132, 132).ToRawColor4(),
+                Ambient = new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1),
+                Specular = new SharpDX.Mathematics.Interop.RawColor4(0.5f, 0.5f, 0.5f, 1),
+                Range = 0,
+                Direction = Vector3.Normalize(new Vector3(0.245f, -0.4f, -0.125f))
+            };
+            d3dDevice.SetLight(1, ref l1);
+            d3dDevice.EnableLight(1, true);
             #endregion
 
             #region Back Light
-            d3dDevice.Lights[2].Type = LightType.Directional;
-            d3dDevice.Lights[2].DiffuseColor = new ColorValue(100, 100, 100, 255);
-            d3dDevice.Lights[2].Diffuse = Color.FromArgb(255, 130, 142, 130);
-            d3dDevice.Lights[2].Ambient = Color.Black;
-            d3dDevice.Lights[2].Specular = Color.Gray;
-            d3dDevice.Lights[2].Range = 0;
-            d3dDevice.Lights[2].Direction = Vector3.Normalize(new Vector3(-0.45f, 1f, 0.25f));
-            d3dDevice.Lights[2].Enabled = true;
+            Light l2 = new Light()
+            {
+                Type = LightType.Directional,
+                Diffuse = Color.FromArgb(255, 130, 142, 130).ToRawColor4(),
+                Ambient = new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1),
+                Specular = new SharpDX.Mathematics.Interop.RawColor4(0.5f, 0.5f, 0.5f, 1),
+                Range = 0,
+                Direction = Vector3.Normalize(new Vector3(-0.45f, 1f, 0.25f))
+            };
+            d3dDevice.SetLight(2, ref l2);
+            d3dDevice.EnableLight(2, true);
+            #endregion
+
+            #region Font Setup
+            onscreenFont = new Font(d3dDevice, 14, 14, FontWeight.DoNotCare, 0, false, FontCharacterSet.Oem, FontPrecision.Default, FontQuality.Default, FontPitchAndFamily.DontCare, "Verdana");
             #endregion
         }
 
