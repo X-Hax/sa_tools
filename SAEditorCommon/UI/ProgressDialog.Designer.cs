e5b2dc623218b4bc88583655662e3d7b6f667e90
PkR <pkr@pkr.pkr>
2020-07-01 20:36:54 +0900

    SAEditorCommon: fix progress dialog at high DPI

--- SAEditorCommon/UI/ProgressDialog.Designer.cs
+++ SAEditorCommon/UI/ProgressDialog.Designer.cs
@@ -28,86 +28,88 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.progressBar = new System.Windows.Forms.ProgressBar();
-			this.labelTask = new System.Windows.Forms.Label();
-			this.labelStep = new System.Windows.Forms.Label();
-			this.checkAutoClose = new System.Windows.Forms.CheckBox();
-			this.buttonOK = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// progressBar
-			// 
-			this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelTask = new System.Windows.Forms.Label();
+            this.labelStep = new System.Windows.Forms.Label();
+            this.checkAutoClose = new System.Windows.Forms.CheckBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.progressBar.Location = new System.Drawing.Point(12, 39);
-			this.progressBar.Name = "progressBar";
-			this.progressBar.Size = new System.Drawing.Size(344, 23);
-			this.progressBar.Step = 1;
-			this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-			this.progressBar.TabIndex = 3;
-			// 
-			// labelTask
-			// 
-			this.labelTask.AutoSize = true;
-			this.labelTask.Location = new System.Drawing.Point(12, 9);
-			this.labelTask.Name = "labelTask";
-			this.labelTask.Size = new System.Drawing.Size(53, 13);
-			this.labelTask.TabIndex = 1;
-			this.labelTask.Text = "labelTask";
-			// 
-			// labelStep
-			// 
-			this.labelStep.AutoSize = true;
-			this.labelStep.Location = new System.Drawing.Point(12, 22);
-			this.labelStep.Name = "labelStep";
-			this.labelStep.Size = new System.Drawing.Size(51, 13);
-			this.labelStep.TabIndex = 2;
-			this.labelStep.Text = "labelStep";
-			// 
-			// checkAutoClose
-			// 
-			this.checkAutoClose.AutoSize = true;
-			this.checkAutoClose.Location = new System.Drawing.Point(12, 72);
-			this.checkAutoClose.Name = "checkAutoClose";
-			this.checkAutoClose.Size = new System.Drawing.Size(127, 17);
-			this.checkAutoClose.TabIndex = 4;
-			this.checkAutoClose.Text = "&Close when complete";
-			this.checkAutoClose.UseVisualStyleBackColor = true;
-			this.checkAutoClose.CheckedChanged += new System.EventHandler(this.checkAutoClose_CheckedChanged);
-			// 
-			// buttonOK
-			// 
-			this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonOK.Location = new System.Drawing.Point(281, 68);
-			this.buttonOK.Name = "buttonOK";
-			this.buttonOK.Size = new System.Drawing.Size(75, 23);
-			this.buttonOK.TabIndex = 5;
-			this.buttonOK.Text = "&OK";
-			this.buttonOK.UseVisualStyleBackColor = true;
-			this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
-			// 
-			// ProgressDialog
-			// 
-			this.AcceptButton = this.buttonOK;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(368, 103);
-			this.ControlBox = false;
-			this.Controls.Add(this.buttonOK);
-			this.Controls.Add(this.checkAutoClose);
-			this.Controls.Add(this.labelStep);
-			this.Controls.Add(this.labelTask);
-			this.Controls.Add(this.progressBar);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "ProgressDialog";
-			this.ShowIcon = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "ProgressDialog";
-			this.Load += new System.EventHandler(this.ProgressDialog_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.progressBar.Location = new System.Drawing.Point(12, 39);
+            this.progressBar.MaximumSize = new System.Drawing.Size(344, 23);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(344, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 3;
+            // 
+            // labelTask
+            // 
+            this.labelTask.AutoSize = true;
+            this.labelTask.Location = new System.Drawing.Point(12, 9);
+            this.labelTask.Name = "labelTask";
+            this.labelTask.Size = new System.Drawing.Size(53, 13);
+            this.labelTask.TabIndex = 1;
+            this.labelTask.Text = "labelTask";
+            // 
+            // labelStep
+            // 
+            this.labelStep.AutoSize = true;
+            this.labelStep.Location = new System.Drawing.Point(12, 22);
+            this.labelStep.Name = "labelStep";
+            this.labelStep.Size = new System.Drawing.Size(51, 13);
+            this.labelStep.TabIndex = 2;
+            this.labelStep.Text = "labelStep";
+            // 
+            // checkAutoClose
+            // 
+            this.checkAutoClose.AutoSize = true;
+            this.checkAutoClose.Location = new System.Drawing.Point(12, 72);
+            this.checkAutoClose.Name = "checkAutoClose";
+            this.checkAutoClose.Size = new System.Drawing.Size(127, 17);
+            this.checkAutoClose.TabIndex = 4;
+            this.checkAutoClose.Text = "&Close when complete";
+            this.checkAutoClose.UseVisualStyleBackColor = true;
+            this.checkAutoClose.CheckedChanged += new System.EventHandler(this.checkAutoClose_CheckedChanged);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(281, 68);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 5;
+            this.buttonOK.Text = "&OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // ProgressDialog
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(368, 103);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.checkAutoClose);
+            this.Controls.Add(this.labelStep);
+            this.Controls.Add(this.labelTask);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressDialog";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ProgressDialog";
+            this.Load += new System.EventHandler(this.ProgressDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
