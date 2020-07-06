cd68e3ae1d79ad0cb36e73e5b793b615bf1b3e72
PkR <pkr@pkr.pkr>
2020-07-06 21:01:37 +0900

    SAEditorCommon: Add resources for the new question mark

--- SAEditorCommon/Properties/Resources.Designer.cs
+++ SAEditorCommon/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SonicRetro.SAModel.SAEditorCommon.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -86,6 +86,26 @@ namespace SonicRetro.SAModel.SAEditorCommon.Properties {
         public static System.Drawing.Bitmap PointBTexture {
             get {
                 object obj = ResourceManager.GetObject("PointBTexture", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        public static byte[] questionmark {
+            get {
+                object obj = ResourceManager.GetObject("questionmark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap questionmark_t {
+            get {
+                object obj = ResourceManager.GetObject("questionmark_t", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
