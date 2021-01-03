138e71df90aa89ad414c4868311b8ecfd12ecf3e
ItsEasyActually <darkspines35@gmail.com>
2021-01-03 01:38:25 -0500

    Preliminary additions for migrating Level Editing work flow

---
+++ SAEditorCommon/ProjectManagement/ObjectTemplate.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using SA_Tools;
+
+namespace SAEditorCommon.StageManagement
+{
+	public enum RotType
+	{
+		XYZ,
+		Y,
+		ZYX,
+		NoRotation
+	}
+
+	public enum SclType
+	{
+		XYZ,
+		X,
+		Y,
+		Z,
+		XY,
+		YZ,
+		XZ,
+		NoScale
+	}
+
+	public class classAltVariables
+	{
+		public int AltRotX { get; set; }
+		public int AltRotY { get; set; }
+		public int AltRotZ { get; set; }
+		public float AltScaleX { get; set; }
+		public float AltScaleY { get; set; }
+		public float AltScaleZ { get; set; }
+	}
+
+	public class classBasicDisplay
+	{
+		[XmlElement("TextureFile", typeof(string), IsNullable=true)]
+		public string TextureFile { get; set; }
+		[XmlElement("Rotation", typeof(RotType), IsNullable=true)]
+		public RotType Rotation { get; set; }
+		[XmlElement("Scale", typeof(SclType), IsNullable=true)]
+		public SclType Scale { get; set; }
+		[XmlElement("AltVariables", typeof(classAltVariables), IsNullable=true)]
+		public classAltVariables AltVariables { get; set; }
+	}
+
+	public class classCodeHandler
+	{
+		[XmlElement("CodeFile", typeof(string), IsNullable = true)]
+		public string CodeFile { get; set; }
+		[XmlElement("NamespaceClass", typeof(string), IsNullable=true)]
+		public string NamepaceClass { get; set; }
+	}
+
+	[XmlRoot(Namespace = "http://www.sonicretro.org")]
+	public class ObjectTemplate
+	{
+		[XmlElement("ItemName", typeof(string))]
+		public string ItemName { get; set; }
+		[XmlElement("CommonName", typeof(string))]
+		public string CommonName { get; set; }
+		[XmlElement("BasicDisplay", typeof(classBasicDisplay), IsNullable=true)]
+		public classBasicDisplay BasicDisplay { get; set; }
+		[XmlElement("CodeHandler", typeof(classCodeHandler), IsNullable=true)]
+		public classCodeHandler CodeHandler { get; set; }
+	}
+}
---
+++ SAEditorCommon/ProjectManagement/StageTemplate.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using SA_Tools;
+
+namespace SAEditorCommon.StageManagement
+{
+	public class StgInfo
+	{
+		[XmlElement("StgID", typeof(string))]
+		public string StgID { get; set; }
+		[XmlElement("Name", typeof(string))]
+		public string Name { get; set; }
+		[XmlElement("ActTotal", typeof(int))]
+		public int ActTotal { get; set; }
+		[XmlElement("StgRoot", typeof(string))]
+		public string StgRoot { get; set; }
+		[XmlElement("ObjTexLists", typeof(string))]
+		public string ObjTexList { get; set; }
+	}
+
+	public class ActInfo
+	{
+		[XmlElement("ActID", typeof(string))]
+		public string ActID { get; set; }
+		[XmlElement("Landtable", typeof(string))]
+		public string Landtable { get; set; }
+		[XmlElement("ActTexList", typeof(string))]
+		public string ActTexList { get; set; }
+		[XmlElement("ObjList", typeof(string))]
+		public string ObjList { get; set; }
+		[XmlElement("KillColli", typeof(string))]
+		public string KillColli { get; set; }
+		[XmlElement("UseCodeItem", typeof(bool))]
+		public bool UseCodeItem { get; set; }
+		[XmlElement("CodeItems", typeof(string), IsNullable = true)]
+		public List<string> CodeItems { get; set; }
+	}
+
+	public class CharInfo
+	{
+		[XmlElement("Name", typeof(string))]
+		public string Name { get; set; }
+		[XmlElement("CharID", typeof(int))]
+		public int CharID { get; set; }
+		[XmlElement("CharTexList", typeof(string))]
+		public string CharTexList { get; set; }
+		[XmlElement("Height", typeof(float))]
+		public float Height { get; set; }
+		[XmlElement("StartPosList", typeof(string))]
+		public string StartPosList { get; set; }
+	}
+
+	[XmlRoot(Namespace = "http://www.sonicretro.org")]
+	public class StageTemplate
+	{
+		[XmlElement("StageInfo", typeof(StgInfo))]
+		public StgInfo StageInfo { get; set; }
+		[XmlElement("ActInfo", typeof(ActInfo))]
+		public List<ActInfo> ActInfos { get; set; }
+	}
+
+	[XmlRoot(Namespace = "http://www.sonicretro.org")]
+	public class CharacterTemplate
+	{
+		[XmlElement("CharData", typeof(CharInfo))]
+		public List<CharInfo> CharData { get; set; }
+	}
+}
