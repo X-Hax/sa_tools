2b4ffefd57783180f3c8695e0fbeb536fd2e258d
PkR <pkr@pkr.pkr>
2021-02-04 02:38:50 +0900

    Configuration: Add STG06 and STG07 to new SA1 split, update split bat file

--- Configuration/SA1/splitnew.bat
+++ Configuration/SA1/splitnew.bat
@@ -1,1 +1,8 @@
 ..\bin\split binary data/1ST_READ.bin newsplit/1ST_READ.ini output/
+..\bin\split binary data/SONICADV/STG01.PRS newsplit/STG01.ini output/
+..\bin\split binary data/SONICADV/STG02.PRS newsplit/STG02.ini output/
+..\bin\split binary data/SONICADV/STG03.PRS newsplit/STG03.ini output/
+..\bin\split binary data/SONICADV/STG04.PRS newsplit/STG04.ini output/
+..\bin\split binary data/SONICADV/STG05.PRS newsplit/STG05.ini output/
+..\bin\split binary data/SONICADV/STG06.PRS newsplit/STG06.ini output/
+..\bin\split binary data/SONICADV/STG07.PRS newsplit/STG07.ini output/
