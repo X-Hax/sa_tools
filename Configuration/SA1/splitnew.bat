1f130b5b0ce971507943b9a5b6dc3160d6904281
PkR <pkr@pkr.pkr>
2021-02-05 00:45:24 +0900

    Configuration: Update new SA1 and SADXPC split

--- Configuration/SA1/splitnew.bat
+++ Configuration/SA1/splitnew.bat
@@ -1,4 +1,5 @@
 ..\bin\split binary data/1ST_READ.bin newsplit/1ST_READ.ini output/
+..\bin\split binary data/SONICADV/STG00.PRS newsplit/STG00.ini output/
 ..\bin\split binary data/SONICADV/STG01.PRS newsplit/STG01.ini output/
 ..\bin\split binary data/SONICADV/STG02.PRS newsplit/STG02.ini output/
 ..\bin\split binary data/SONICADV/STG03.PRS newsplit/STG03.ini output/
@@ -8,3 +9,5 @@
 ..\bin\split binary data/SONICADV/STG07.PRS newsplit/STG07.ini output/
 ..\bin\split binary data/SONICADV/STG08.PRS newsplit/STG08.ini output/
 ..\bin\split binary data/SONICADV/STG09.PRS newsplit/STG09.ini output/
+..\bin\split binary data/SONICADV/STG10.PRS newsplit/STG10.ini output/
+..\bin\split binary data/SONICADV/STG12.PRS newsplit/STG12.ini output/
