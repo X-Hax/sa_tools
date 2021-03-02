a9bc577920d21e971e342c7c9fbbe107960a5c42
PkR <pkr@pkr.pkr>
2021-03-03 00:29:08 +0900

    Configuration: Add STG03-05 to SADX Gamecube split

--- Configuration/SADXGC/splitgc.bat
+++ Configuration/SADXGC/splitgc.bat
@@ -1,3 +1,6 @@
 ..\bin\split binary data/_Main.rel _Main.ini output/
 ..\bin\split binary data/_STG01.rel _STG01.ini output/
 ..\bin\split binary data/_STG02.rel _STG02.ini output/
+..\bin\split binary data/_STG03.rel _STG03.ini output/
+..\bin\split binary data/_STG04.rel _STG04.ini output/
+..\bin\split binary data/_STG05.rel _STG05.ini output/
