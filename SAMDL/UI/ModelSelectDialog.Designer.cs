f45d160957c88f21861178f67c4ee26930f0013f
ItsEasyActually <darkspines35@gmail.com>
2021-09-21 22:05:03 -0400

    SAMDL: Setting Model Select UI anchors. Allows scaling the window to scale the Model List and Category Dropdowns properly.

--- SAMDL/UI/ModelSelectDialog.Designer.cs
+++ SAMDL/UI/ModelSelectDialog.Designer.cs
@@ -29,107 +29,100 @@ namespace SAModel.SAMDL
 		/// </summary>
 		private void InitializeComponent()
 		{
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModelSelectDialog));
-            this.buttonOK = new System.Windows.Forms.Button();
-            this.listModels = new System.Windows.Forms.ListBox();
-            this.comboCategories = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.buttonCancel = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // buttonOK
-            // 
-            this.buttonOK.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOK.Enabled = false;
-            this.buttonOK.Location = new System.Drawing.Point(321, 674);
-            this.buttonOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new System.Drawing.Size(112, 35);
-            this.buttonOK.TabIndex = 11;
-            this.buttonOK.Text = "&OK";
-            this.buttonOK.UseVisualStyleBackColor = true;
-            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
-            // 
-            // listModels
-            // 
-            this.listModels.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.listModels.FormattingEnabled = true;
-            this.listModels.ItemHeight = 20;
-            this.listModels.Location = new System.Drawing.Point(13, 103);
-            this.listModels.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.listModels.Name = "listModels";
-            this.listModels.Size = new System.Drawing.Size(420, 564);
-            this.listModels.TabIndex = 10;
-            this.listModels.SelectedIndexChanged += new System.EventHandler(this.listModels_SelectedIndexChanged);
-            this.listModels.DoubleClick += new System.EventHandler(this.listModels_DoubleClick);
-            // 
-            // comboCategories
-            // 
-            this.comboCategories.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.comboCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboCategories.FormattingEnabled = true;
-            this.comboCategories.Location = new System.Drawing.Point(13, 41);
-            this.comboCategories.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.comboCategories.Name = "comboCategories";
-            this.comboCategories.Size = new System.Drawing.Size(420, 28);
-            this.comboCategories.TabIndex = 8;
-            this.comboCategories.SelectedIndexChanged += new System.EventHandler(this.comboCategories_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 78);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 20);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Model:";
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 16);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 20);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Category:";
-            // 
-            // buttonCancel
-            // 
-            this.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(201, 674);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(112, 35);
-            this.buttonCancel.TabIndex = 14;
-            this.buttonCancel.Text = "&Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
-            // 
-            // ModelSelectDialog
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 720);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.buttonOK);
-            this.Controls.Add(this.listModels);
-            this.Controls.Add(this.comboCategories);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "ModelSelectDialog";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Model Select";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModelSelectDialog));
+			this.buttonOK = new System.Windows.Forms.Button();
+			this.listModels = new System.Windows.Forms.ListBox();
+			this.comboCategories = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// buttonOK
+			// 
+			this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.buttonOK.Enabled = false;
+			this.buttonOK.Location = new System.Drawing.Point(214, 438);
+			this.buttonOK.Name = "buttonOK";
+			this.buttonOK.Size = new System.Drawing.Size(75, 23);
+			this.buttonOK.TabIndex = 11;
+			this.buttonOK.Text = "&OK";
+			this.buttonOK.UseVisualStyleBackColor = true;
+			this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+			// 
+			// listModels
+			// 
+			this.listModels.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listModels.FormattingEnabled = true;
+			this.listModels.Location = new System.Drawing.Point(9, 67);
+			this.listModels.Name = "listModels";
+			this.listModels.Size = new System.Drawing.Size(281, 368);
+			this.listModels.TabIndex = 10;
+			this.listModels.SelectedIndexChanged += new System.EventHandler(this.listModels_SelectedIndexChanged);
+			this.listModels.DoubleClick += new System.EventHandler(this.listModels_DoubleClick);
+			// 
+			// comboCategories
+			// 
+			this.comboCategories.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboCategories.FormattingEnabled = true;
+			this.comboCategories.Location = new System.Drawing.Point(9, 27);
+			this.comboCategories.Name = "comboCategories";
+			this.comboCategories.Size = new System.Drawing.Size(281, 21);
+			this.comboCategories.TabIndex = 8;
+			this.comboCategories.SelectedIndexChanged += new System.EventHandler(this.comboCategories_SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 51);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(39, 13);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Model:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(52, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Category:";
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonCancel.Location = new System.Drawing.Point(134, 438);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 14;
+			this.buttonCancel.Text = "&Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+			// 
+			// ModelSelectDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(299, 468);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonOK);
+			this.Controls.Add(this.listModels);
+			this.Controls.Add(this.comboCategories);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "ModelSelectDialog";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Model Select";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
