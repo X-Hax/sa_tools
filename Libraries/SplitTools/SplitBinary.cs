4cdc532065f57ea2e36120ad76a5563b2333d0de
PkR <PiKeyAr@users.noreply.github.com>
2021-09-26 21:55:20 +0900

    SplitTools: Remove unused code in SplitBinary

--- Libraries/SplitTools/SplitBinary.cs
+++ Libraries/SplitTools/SplitBinary.cs
@@ -1406,53 +1406,6 @@ namespace SplitTools.Split
             }
             SplitSingle(itemname == "" ? itemType + "_" + address.ToString("X8") : itemname, info, Path.GetPathRoot(outputFilename), File.ReadAllBytes(dataFileName), imageBase, new Dictionary<int, string>(), gameBase, null, nometa, nolabel);
         }
-
-        static string MakeRelativePath(string workingDirectory, string fullPath)
-        {
-            string result = string.Empty;
-            int offset;
-
-            // this is the easy case.  The file is inside of the working directory.
-            if (fullPath.StartsWith(workingDirectory))
-            {
-                return fullPath.Substring(workingDirectory.Length + 1);
-            }
-
-            // the hard case has to back out of the working directory
-            string[] baseDirs = workingDirectory.Split(new char[] { ':', '\\', '/' });
-            string[] fileDirs = fullPath.Split(new char[] { ':', '\\', '/' });
-
-            // if we failed to split (empty strings?) or the drive letter does not match
-            if (baseDirs.Length <= 0 || fileDirs.Length <= 0 || baseDirs[0] != fileDirs[0])
-            {
-                // can't create a relative path between separate harddrives/partitions.
-                return fullPath;
-            }
-
-            // skip all leading directories that match
-            for (offset = 1; offset < baseDirs.Length; offset++)
-            {
-                if (baseDirs[offset] != fileDirs[offset])
-                    break;
-            }
-
-            // back out of the working directory
-            for (int i = 0; i < (baseDirs.Length - offset); i++)
-            {
-                result += "..\\";
-            }
-
-            // step into the file path
-            for (int i = offset; i < fileDirs.Length - 1; i++)
-            {
-                result += fileDirs[i] + "\\";
-            }
-
-            // append the file
-            result += fileDirs[fileDirs.Length - 1];
-
-            return result;
-        }
     }
 
     public enum SplitERRORVALUE
