f93b3966e92e4ef6a332d381e9e1f59cd307bc9c
PkR <PiKeyAr@users.noreply.github.com>
2021-11-07 03:50:47 +0900

    SAToolsHub: Fixed update progress dialog at High DPI

--- SAToolsHub/Updater/ProgressDialog.Designer.cs
+++ SAToolsHub/Updater/ProgressDialog.Designer.cs
@@ -28,75 +28,75 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.progressBar = new System.Windows.Forms.ProgressBar();
-			this.labelTask = new System.Windows.Forms.Label();
-			this.labelStep = new System.Windows.Forms.Label();
-			this.buttonCancel = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// progressBar
-			// 
-			this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.progressBar.Location = new System.Drawing.Point(12, 39);
-			this.progressBar.Maximum = 2147483646;
-			this.progressBar.Name = "progressBar";
-			this.progressBar.Size = new System.Drawing.Size(344, 23);
-			this.progressBar.Step = 1;
-			this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-			this.progressBar.TabIndex = 3;
-			// 
-			// labelTask
-			// 
-			this.labelTask.AutoEllipsis = true;
-			this.labelTask.Location = new System.Drawing.Point(12, 9);
-			this.labelTask.Name = "labelTask";
-			this.labelTask.Size = new System.Drawing.Size(344, 13);
-			this.labelTask.TabIndex = 1;
-			this.labelTask.Text = "labelTask";
-			// 
-			// labelStep
-			// 
-			this.labelStep.AutoEllipsis = true;
-			this.labelStep.Location = new System.Drawing.Point(12, 23);
-			this.labelStep.Name = "labelStep";
-			this.labelStep.Size = new System.Drawing.Size(344, 13);
-			this.labelStep.TabIndex = 2;
-			this.labelStep.Text = "labelStep";
-			// 
-			// buttonCancel
-			// 
-			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.buttonCancel.Location = new System.Drawing.Point(281, 68);
-			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
-			this.buttonCancel.TabIndex = 4;
-			this.buttonCancel.Text = "&Cancel";
-			this.buttonCancel.UseVisualStyleBackColor = true;
-			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
-			// 
-			// ProgressDialog
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.buttonCancel;
-			this.ClientSize = new System.Drawing.Size(368, 103);
-			this.ControlBox = false;
-			this.Controls.Add(this.buttonCancel);
-			this.Controls.Add(this.labelStep);
-			this.Controls.Add(this.labelTask);
-			this.Controls.Add(this.progressBar);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "ProgressDialog";
-			this.ShowIcon = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "ProgressDialog";
-			this.Load += new System.EventHandler(this.ProgressDialog_Load);
-			this.ResumeLayout(false);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelTask = new System.Windows.Forms.Label();
+            this.labelStep = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 39);
+            this.progressBar.Maximum = 2147483646;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(344, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 3;
+            // 
+            // labelTask
+            // 
+            this.labelTask.AutoEllipsis = true;
+            this.labelTask.Location = new System.Drawing.Point(12, 9);
+            this.labelTask.Name = "labelTask";
+            this.labelTask.Size = new System.Drawing.Size(344, 13);
+            this.labelTask.TabIndex = 1;
+            this.labelTask.Text = "labelTask";
+            // 
+            // labelStep
+            // 
+            this.labelStep.AutoEllipsis = true;
+            this.labelStep.Location = new System.Drawing.Point(12, 23);
+            this.labelStep.Name = "labelStep";
+            this.labelStep.Size = new System.Drawing.Size(344, 13);
+            this.labelStep.TabIndex = 2;
+            this.labelStep.Text = "labelStep";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonCancel.Location = new System.Drawing.Point(281, 68);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "&Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // ProgressDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(368, 103);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelStep);
+            this.Controls.Add(this.labelTask);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressDialog";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ProgressDialog";
+            this.Load += new System.EventHandler(this.ProgressDialog_Load);
+            this.ResumeLayout(false);
 
 		}
 
