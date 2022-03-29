a23fd828effedca78210e1200cca04eb98e4f807
Sora <sora.x@hotmail.fr>
2022-03-29 03:46:36 +0200

    LevelConverter: Fix flags when converting SA1 Levels to SA2.

--- CommandLine/LevelConverter/Program.cs
+++ CommandLine/LevelConverter/Program.cs
@@ -30,6 +30,52 @@ namespace LevelConverter
 						List<COL> newcollist = new List<COL>();
 						foreach (COL col in level.COL.Where((col) => col.Model != null && col.Model.Attach != null))
 						{
+							//fix flags differences
+							if ((col.SurfaceFlags & SA1SurfaceFlags.UseSkyDrawDistance) == SA1SurfaceFlags.UseSkyDrawDistance)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.UseSkyDrawDistance;
+							}
+
+							if ((col.SurfaceFlags & SA1SurfaceFlags.Diggable) == SA1SurfaceFlags.Diggable)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.Diggable;
+								col.SurfaceFlags |= (SA1SurfaceFlags)SA2SurfaceFlags.Diggable;
+							}
+
+							if ((col.SurfaceFlags & SA1SurfaceFlags.Stairs) == SA1SurfaceFlags.Stairs)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.Stairs;
+								col.SurfaceFlags |= (SA1SurfaceFlags)SA2SurfaceFlags.Stairs;
+							}
+
+							if ((col.SurfaceFlags & SA1SurfaceFlags.Waterfall) == SA1SurfaceFlags.Waterfall)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.Waterfall;
+							}
+
+							if ((col.SurfaceFlags & SA1SurfaceFlags.Hurt) == SA1SurfaceFlags.Hurt)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.Hurt;
+								col.SurfaceFlags |= (SA1SurfaceFlags)SA2SurfaceFlags.Hurt;
+							}
+
+							if ((col.SurfaceFlags & SA1SurfaceFlags.Unclimbable) == SA1SurfaceFlags.Unclimbable)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.Unclimbable;
+								col.SurfaceFlags |= (SA1SurfaceFlags)SA2SurfaceFlags.Unclimbable;
+							}
+
+							if ((col.SurfaceFlags & SA1SurfaceFlags.CannotLand) == SA1SurfaceFlags.CannotLand)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.CannotLand;
+								col.SurfaceFlags |= (SA1SurfaceFlags)SA2SurfaceFlags.CannotLand;
+							}
+
+							if ((col.SurfaceFlags & SA1SurfaceFlags.LowDepth) == SA1SurfaceFlags.LowDepth)
+							{
+								col.SurfaceFlags &= ~SA1SurfaceFlags.LowDepth;
+							}
+
 							if ((col.SurfaceFlags & SA1SurfaceFlags.Visible) == SA1SurfaceFlags.Visible)
 							{
 								COL newcol = new COL() { Bounds = col.Bounds };
