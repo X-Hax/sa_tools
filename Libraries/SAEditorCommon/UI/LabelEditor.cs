ee8c924473af4d8ae932a8867b26be70b738f1dd
PiKeyAr <PiKeyAr@users.noreply.github.com>
2022-06-12 02:41:42 +0900

    LabelEditor: Add numeric support and other stuff

--- Libraries/SAEditorCommon/UI/LabelEditor.cs
+++ Libraries/SAEditorCommon/UI/LabelEditor.cs
@@ -5,27 +5,38 @@ namespace SAModel.SAEditorCommon.UI
 {
 	public partial class LabelEditor : Form
 	{
-		private string original;
-		public string result;
+		private string OriginalValue;
+		private bool IsNumeric;
+		public string Result;
 
-		public LabelEditor(string label)
+		public LabelEditor(string originalValue, string headerText = "", bool numeric = false)
 		{
+			IsNumeric = numeric;
 			InitializeComponent();
-			Text = "Edit Label: " + label;
-			textBox1.Text = label;
-			original = label;
+			Text = !string.IsNullOrEmpty(headerText) ? headerText : (numeric ? "Edit Value: " : "Edit Label: " + originalValue);
+			textBox1.Text = originalValue;
+			OriginalValue = originalValue;
 			textBox1.SelectAll();
 			textBox1.Focus();
 		}
 
 		private void buttonReset_Click(object sender, EventArgs e)
 		{
-			textBox1.Text = original;
+			textBox1.Text = OriginalValue;
 		}
 
 		private void LabelEditor_FormClosing(object sender, FormClosingEventArgs e)
 		{
-			result = textBox1.Text;
+			if (IsNumeric)
+			{
+				if (!int.TryParse(textBox1.Text, out int result))
+				{
+					MessageBox.Show(this, "Unable to parse text '" + textBox1.Text + "' as an integer value. Enter a valid number and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					e.Cancel = true;
+					return;
+				}
+			}
+			Result = textBox1.Text;
 		}
 
 		private void textBox1_KeyDown(object sender, KeyEventArgs e)
