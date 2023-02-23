f903c16755b93e0f3ae01a65c0fa8f37a816c203
PkR <PiKeyAr@users.noreply.github.com>
2023-02-23 22:24:36 +0900

    SAToolsInstaller: Update to download .NET 7.0 instead of 5.0

--- SAToolsInstaller/Program.cs
+++ SAToolsInstaller/Program.cs
@@ -22,23 +22,23 @@ namespace SAToolsInstaller
 				Directory.CreateDirectory(".updates");
 			var finder = new FrameworkFinder(Environment.Is64BitOperatingSystem);
 			var resolver = new DependencyResolver(finder);
-			var framework = new Framework("Microsoft.WindowsDesktop.App", "5.0.0");
-			var options = new RuntimeOptions("net5.0", framework, RollForwardPolicy.Minor);
+			var framework = new Framework("Microsoft.WindowsDesktop.App", "7.0.3");
+			var options = new RuntimeOptions("net7.0", framework, RollForwardPolicy.Minor);
 			var result = resolver.Resolve(options);
 
 			// Check if dependencies are missing.
 			if (!result.Available)
 			{
-				if (MessageBox.Show(".NET 5.0 Desktop Runtime is not installed! Would you like to install it now?", "SA Tools Installer", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
+				if (MessageBox.Show(".NET 7.0 Desktop Runtime is not installed! Would you like to install it now?", "SA Tools Installer", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
 				{
-					MessageBox.Show("SA Tools cannot be installed without .NET 5.", "SA Tools Installer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					MessageBox.Show("SA Tools cannot be installed without .NET 7.", "SA Tools Installer", MessageBoxButtons.OK, MessageBoxIcon.Error);
 					return;
 				}
 				FrameworkDownloader frameworkDownloader = new FrameworkDownloader(framework.NuGetVersion, framework.FrameworkName);
 				string url = frameworkDownloader.GetDownloadUrlAsync(Environment.Is64BitOperatingSystem ? Architecture.Amd64 : Architecture.x86).GetAwaiter().GetResult();
 				SAToolsHub.Updater.UpdaterWebClient client = new SAToolsHub.Updater.UpdaterWebClient();
-				client.DownloadFile(url, "NET5Install.exe");
-				Process.Start(new ProcessStartInfo("NET5Install.exe", "/install /passive /norestart") { UseShellExecute = true, Verb = "runas" }).WaitForExit();
+				client.DownloadFile(url, "NET7Install.exe");
+				Process.Start(new ProcessStartInfo("NET7Install.exe", "/install /passive /norestart") { UseShellExecute = true, Verb = "runas" }).WaitForExit();
 			}
 			SAToolsHub.Updater.LoaderDownloadDialog mainForm = new SAToolsHub.Updater.LoaderDownloadDialog(Environment.Is64BitOperatingSystem ? "http://mm.reimuhakurei.net/SA%20Tools%20x64.7z" : "http://mm.reimuhakurei.net/SA%20Tools%20x86.7z", ".updates");
 			Application.Run(mainForm);
