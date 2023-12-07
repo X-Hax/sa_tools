e692a80e0fc39f60e7364a899fd529a8cc5ffcfe
PkR <PiKeyAr@users.noreply.github.com>
2023-12-07 23:31:40 +0900

    SAToolsHub, SAEditorCommon: Updated decomp resource support

---
+++ Libraries/SAEditorCommon/ProjectManagement/Decomp.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace SAModel.SAEditorCommon.ProjectManagement
+{
+	public static class NJAExporter
+	{
+		public static void GenerateDup(string filename, string duppath)
+		{
+			string outpath = Path.Combine(Path.GetDirectoryName(filename), duppath);
+			LandTable land = LandTable.LoadFromFile(filename);
+			List<Attach> atts = new List<Attach>();
+			List<NJS_MOTION> mots = new List<NJS_MOTION>();
+			List<NJS_OBJECT> dupmodels = new List<NJS_OBJECT>();
+			List<NJS_ACTION> dupactions = new List<NJS_ACTION>();
+			foreach (COL col in land.COL)
+			{
+				if (CheckDuplicateObject(col.Model) && !dupmodels.Contains(col.Model))
+				{
+					dupmodels.Add(col.Model);
+				}
+			}
+			// Make a list of duplicate models
+			if (dupmodels.Count > 0)
+			{
+				TextWriter tw_nja = File.CreateText(Path.Combine(outpath, "dupmodel.dup"));
+				foreach (NJS_OBJECT obj in dupmodels)
+				{
+					ObjectToNJA(obj, tw_nja);
+				}
+				tw_nja.Flush();
+				tw_nja.Close();
+			}
+			if (land.Anim != null)
+			{
+				foreach (GeoAnimData geo in land.Anim)
+				{
+					NJS_ACTION act = new NJS_ACTION(geo.Model, geo.Animation);
+					act.Name = geo.Animation.ActionName;
+					if (CheckDuplicateAction(act) && !dupactions.Contains(act))
+					{
+						dupactions.Add(act);
+					}
+				}
+				// Make a list of duplicate actions
+				if (dupactions.Count > 0)
+				{
+					TextWriter twa_nja = File.CreateText(Path.Combine(outpath, "dupmotion.dup"));
+					foreach (NJS_ACTION act in dupactions)
+					{
+						ActionToNJA(act, twa_nja);
+					}
+					twa_nja.Flush();
+					twa_nja.Close();
+				}
+			}
+		}
+
+		static bool CheckDuplicateObject(NJS_OBJECT obj)
+		{
+			// This relies on the label of the OBJECT being different from that of the MODEL.
+			// If the MODEL is being reused, usually the OBJECT label has a number at the end.
+			// If the OBJECT has the number and the MODEL doesn't, it's most likely a duplicate.
+			return obj.Name.Substring(obj.Name.Length - 3, 3) != obj.Attach.Name.Substring(obj.Attach.Name.Length - 3, 3);
+		}
+
+		static bool CheckDuplicateAction(NJS_ACTION act)
+		{
+			// Same as above but for ACTIONs using the same MOTION
+			return act.Name.Substring(act.Name.Length - 3, 3) != act.Animation.Name.Substring(act.Animation.Name.Length - 3, 3);
+		}
+
+		static void ActionToNJA(NJS_ACTION act, TextWriter writer)
+		{
+			writer.WriteLine("ACTION {0}[]", act.Name);
+			writer.WriteLine("START");
+			writer.WriteLine("ObjectHead      {0},", act.Model.Name);
+			writer.WriteLine("Motion          " + act.Animation.Name);
+			writer.WriteLine("END" + Environment.NewLine);
+		}
+
+		static void ObjectToNJA(NJS_OBJECT obj, TextWriter writer)
+		{
+			writer.Write("OBJECT      ");
+			writer.Write(obj.Name);
+			writer.WriteLine("[]");
+			writer.WriteLine("START");
+			writer.WriteLine("EvalFlags ( 0x" + ((int)obj.GetFlags()).ToString("x8") + " ),");
+			writer.WriteLine("Model       " + (obj.Attach != null ? obj.Attach.Name : "NULL") + ",");
+			writer.WriteLine("OPosition  {0},", obj.Position.ToNJA());
+			writer.WriteLine("OAngle     ( " + ((float)obj.Rotation.X / 182.044f).ToNJA() + ", " + ((float)obj.Rotation.Y / 182.044f).ToNJA() + ", " + ((float)obj.Rotation.Z / 182.044f).ToNJA() + " ),");
+			writer.WriteLine("OScale     {0},", obj.Scale.ToNJA());
+			writer.WriteLine("Child       " + (obj.Children.Count > 0 ? obj.Children[0].Name : "NULL") + ",");
+			writer.WriteLine("Sibling     " + (obj.Sibling != null ? obj.Sibling.Name : "NULL") + ",");
+			writer.WriteLine("END" + Environment.NewLine);
+		}
+	}
+}
--- SAToolsHub/Forms/newProj.cs
+++ SAToolsHub/Forms/newProj.cs
@@ -9,7 +9,6 @@ using SAModel.SAEditorCommon.ModManagement;
 using SAModel.SAEditorCommon.ProjectManagement;
 using System.Xml;
 using SplitTools.Split;
-using SAModel;
 using SplitTools;
 
 namespace SAToolsHub
@@ -550,16 +549,24 @@ namespace SAToolsHub
 			// Process and clean up non-NJA stuff
 			if (isNJA)
 			{
+				string duplistpath = Path.Combine(iniFolder, "duppath.txt");
+				Dictionary<string, string> duplist = new Dictionary<string, string>();
+				if (File.Exists(duplistpath))
+					duplist = IniSerializer.Deserialize<Dictionary<string, string>>(duplistpath);
 				progress.SetTask("Processing and cleaning decomp files");
+				progress.SetStep("");
 				string[] files = Directory.GetFiles(projFolder, "*.*", SearchOption.AllDirectories);
 				foreach (string file in files)
 				{
-					progress.SetStep("Processing " + file);
+
 					switch (Path.GetExtension(file).ToLowerInvariant())
 					{
 						case ".sa1lvl":
+							string duppath = Path.Combine(Path.GetDirectoryName(file));
+							if (duplist.ContainsKey(Path.GetFileName(file)))
+								duppath = Path.Combine(Path.GetDirectoryName(file), duplist[Path.GetFileName(file)]);
 							StructConversion.ConvertFileToText(file, StructConversion.TextType.NJA, file[..file.LastIndexOf(".")], false, true);
-							GenerateDup(file);
+                            SAModel.SAEditorCommon.ProjectManagement.NJAExporter.GenerateDup(file, duppath);
 							File.Delete(file);
 							break;
 						case ".sa1mdl":
@@ -597,106 +604,6 @@ namespace SAToolsHub
 				progress.StepProgress();
 			}
 			return ProjectSplitResult.Success;
-		}
-		#endregion
-
-		#region dupmodel.dup generation for lantables (NJA)
-		static void GenerateDup(string filename)
-		{
-			string outname = Path.GetFileNameWithoutExtension(filename);
-			string outpath = Path.GetDirectoryName(filename);
-			LandTable land = LandTable.LoadFromFile(filename);
-			List<Attach> atts = new List<Attach>();
-			List<NJS_OBJECT> dupmodels = new List<NJS_OBJECT>();
-			foreach (COL col in land.COL)
-			{
-				if (CheckDuplicateObject(col.Model) && !dupmodels.Contains(col.Model))
-				{
-					dupmodels.Add(col.Model);
-					Console.WriteLine(col.Model.Name);
-				}
-			}
-			// Make a list of duplicate models
-			TextWriter tw = File.CreateText(Path.Combine(outpath, outname + "_dupmodel_c.dup"));
-			TextWriter tw_nja = File.CreateText(Path.Combine(outpath, outname + "_dupmodel.dup"));
-			foreach (NJS_OBJECT obj in dupmodels)
-			{
-				ObjectToC(obj, tw);
-				ObjectToNJA(obj, tw_nja);
-			}
-			tw.Flush();
-			tw.Close();
-			tw_nja.Flush();
-			tw_nja.Close();
-			Console.WriteLine("Finished!");
-		}
-
-		static bool CheckDuplicateObject(NJS_OBJECT obj)
-		{
-			// This relies on the label of the OBJECT being different from that of the MODEL.
-			// If the MODEL is being reused, usually the OBJECT label has a number at the end.
-			// If the OBJECT has the number and the MODEL doesn't, it's most likely a duplicate.
-			return obj.Name.Substring(obj.Name.Length - 3, 3) != obj.Attach.Name.Substring(obj.Attach.Name.Length - 3, 3);
-		}
-
-		static void ObjectToNJA(NJS_OBJECT obj, TextWriter writer)
-		{
-			writer.Write("OBJECT      ");
-			writer.Write(obj.Name);
-			writer.WriteLine("[]");
-			writer.WriteLine("START");
-			writer.WriteLine("EvalFlags ( 0x" + ((int)obj.GetFlags()).ToString("x8") + " ),");
-			writer.WriteLine("Model       " + (obj.Attach != null ? obj.Attach.Name : "NULL") + ",");
-			writer.WriteLine("OPosition  {0},", obj.Position.ToNJA());
-			writer.WriteLine("OAngle     ( " + ((float)obj.Rotation.X / 182.044f).ToNJA() + ", " + ((float)obj.Rotation.Y / 182.044f).ToNJA() + ", " + ((float)obj.Rotation.Z / 182.044f).ToNJA() + " ),");
-			writer.WriteLine("OScale     {0},", obj.Scale.ToNJA());
-			writer.WriteLine("Child       " + (obj.Children.Count > 0 ? obj.Children[0].Name : "NULL") + ",");
-			writer.WriteLine("Sibling     " + (obj.Sibling != null ? obj.Sibling.Name : "NULL") + ",");
-			writer.WriteLine("END" + Environment.NewLine);
-			writer.WriteLine("OBJECT_END");
-			// Probably not necessary for LandTable
-			/* 
-			if (obj.Parent == null)
-			{
-				writer.WriteLine(Environment.NewLine + "DEFAULT_START");
-				writer.WriteLine(Environment.NewLine + "#ifndef DEFAULT_OBJECT_NAME");
-				writer.WriteLine("#define DEFAULT_OBJECT_NAME " + obj.Name);
-				writer.WriteLine("#endif");
-				writer.WriteLine(Environment.NewLine + "DEFAULT_END");
-			}
-			*/
-		}
-
-		static void ObjectToC(NJS_OBJECT obj, TextWriter writer)
-		{
-			// ToStruct modified for array-like export
-			writer.Write("NJS_OBJECT ");
-			writer.Write(obj.Name);
-			writer.Write("[1] = { { ");
-			writer.Write(((StructEnums.NJD_EVAL)obj.GetFlags()).ToString().Replace(", ", " | "));
-			writer.Write(", ");
-			writer.Write(obj.Attach != null ? obj.Attach.Name : "NULL");
-			foreach (float value in obj.Position.ToArray())
-			{
-				writer.Write(", ");
-				writer.Write(value.ToC());
-			}
-			foreach (int value in obj.Rotation.ToArray())
-			{
-				writer.Write(", ");
-				writer.Write(value.ToCHex());
-			}
-			foreach (float value in obj.Scale.ToArray())
-			{
-				writer.Write(", ");
-				writer.Write(value.ToC());
-			}
-			writer.Write(", ");
-			writer.Write(obj.Children.Count > 0 ? "&" + obj.Children[0].Name : "NULL");
-			writer.Write(", ");
-			writer.Write(obj.Sibling != null ? "&" + obj.Sibling.Name : "NULL");
-			writer.Write(" }");
-			writer.WriteLine(" };");
 		}
 		#endregion
 
