d7e3342e52cbcc065a4ad89cf7031de367e0126d
ItsEasyActually <darkspines35@gmail.com>
2024-02-25 20:03:03 -0500

    Removing unused functions.

--- Libraries/ArchiveLib/SkiesOfArcadia.cs
+++ Libraries/ArchiveLib/SkiesOfArcadia.cs
@@ -534,28 +534,6 @@ namespace ArchiveLib
 				Console.WriteLine("Unable to read archive.");
 		}
 
-		static public List<int> SearchBytePattern(byte[] pattern, byte[] bytes)
-		{
-			List<int> positions = new List<int>();
-			int patternLength = pattern.Length;
-			int totalLength = bytes.Length;
-			byte firstMatchByte = pattern[0];
-			for (int i = 0; i < totalLength; i++)
-			{
-				if (firstMatchByte == bytes[i] && totalLength - i >= patternLength)
-				{
-					byte[] match = new byte[patternLength];
-					Array.Copy(bytes, i, match, 0, patternLength);
-					if (match.SequenceEqual<byte>(pattern))
-					{
-						positions.Add(i);
-						i += patternLength - 1;
-					}
-				}
-			}
-			return positions;
-		}
-
 		public override byte[] GetBytes()
 		{
 			throw new NotImplementedException();
