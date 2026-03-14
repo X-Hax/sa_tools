ce2ca3c6387c91a2c42364093fd6f8b37e4840bc
PkR <pikeyar@pm.me>
2026-03-14 15:13:56 +0900

    SALVL: Texture log change

--- SALVL/MainForm/Loading.cs
+++ SALVL/MainForm/Loading.cs
@@ -259,7 +259,6 @@ namespace SAModel.SALVL
 				else
 					textureFallbackPath = Path.Combine(systemFallback, "PRS", pvmName) + extension;
 				BMPInfo[] textureBitmaps = TextureArchive.GetTextures(ProjectFunctions.ModPathOrGameFallback(texturePath, textureFallbackPath), out bool hasNames);
-				log.Add("Loading textures: " + ProjectFunctions.ModPathOrGameFallback(texturePath, textureFallbackPath));
 				Texture[] d3dTextures;
 				if (textureBitmaps != null)
 				{
@@ -269,6 +268,7 @@ namespace SAModel.SALVL
 
 					LevelData.TextureBitmaps.Add(pvmName, textureBitmaps);
 					LevelData.Textures.Add(pvmName, d3dTextures);
+					log.Add("Loaded textures: " + ProjectFunctions.ModPathOrGameFallback(texturePath, textureFallbackPath));
 				}
 				else
 				{
